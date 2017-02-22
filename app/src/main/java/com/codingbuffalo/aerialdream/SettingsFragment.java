7e99f9cc03f8ad0be60abaefa903a7ac852f05bb
Daniel Cachapa <cachapa@gmail.com>
2017-02-22 12:36:29 +0100

    Open screensaver settings on devices which allow it

--- app/src/main/java/com/codingbuffalo/aerialdream/SettingsFragment.java
+++ app/src/main/java/com/codingbuffalo/aerialdream/SettingsFragment.java
@@ -1,11 +1,37 @@
 package com.codingbuffalo.aerialdream;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v14.preference.PreferenceFragment;
+import android.support.v7.preference.Preference;
 
-public class SettingsFragment extends PreferenceFragment {
+import java.util.List;
+
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener {
+    public static final String SETTINGS = "android.settings.SETTINGS";
+    public static final String SCREENSAVER_SETTINGS = "android.settings.DREAM_SETTINGS";
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.settings, rootKey);
+        findPreference("system_options").setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        // Some devices (e.g. NVidia Shield) do not support the screensaver intent
+        // Check if the intent is available and if not, open the normal preferences
+        Intent intent = new Intent(SCREENSAVER_SETTINGS);
+        PackageManager manager = getActivity().getPackageManager();
+        List<ResolveInfo> infos = manager.queryIntentActivities(intent, 0);
+
+        if (infos.isEmpty()) {
+            intent = new Intent(SETTINGS);
+        }
+
+        startActivity(intent);
+        return true;
     }
 }
