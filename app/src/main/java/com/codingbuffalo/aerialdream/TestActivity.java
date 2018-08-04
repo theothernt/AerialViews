1e20b7e7bacc31f681aa71f65ec115067deb041b
Maura Lynch <40187540+birdsof@users.noreply.github.com>
2018-08-03 23:57:22 -0400

    Add immersive sticky flag to auto hide nav bar

--- app/src/main/java/com/codingbuffalo/aerialdream/TestActivity.java
+++ app/src/main/java/com/codingbuffalo/aerialdream/TestActivity.java
@@ -23,6 +23,7 @@ public class TestActivity extends Activity {
         if (hasFocus) {
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_FULLSCREEN);
         }
