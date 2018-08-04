c5977e7dc059b72fac1916e51a09a733f7211b18
Maura Lynch <40187540+birdsof@users.noreply.github.com>
2018-08-03 23:40:31 -0400

    Hide navigation bar on window focus gain

--- app/src/main/java/com/codingbuffalo/aerialdream/TestActivity.java
+++ app/src/main/java/com/codingbuffalo/aerialdream/TestActivity.java
@@ -15,10 +15,17 @@ public class TestActivity extends Activity {
 
         videoController = new VideoController(this);
         setContentView(videoController.getView());
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+    
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {         
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
     }
 
     @Override
