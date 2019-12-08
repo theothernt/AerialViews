00c6f8d3494dd99963488d4c1ffa015646ebc235
Daniel Cachapa <cachapa@gmail.com>
2019-12-08 10:51:42 +0100

    Update tools and dependencies

---
+++ app/src/main/java/com/codingbuffalo/aerialdream/data/Apple2019Repository.java
@@ -0,0 +1,19 @@
+package com.codingbuffalo.aerialdream.data;
+
+import android.content.Context;
+
+import com.codingbuffalo.aerialdream.R;
+
+import java.util.List;
+
+public class Apple2018Repository extends VideoRepository {
+    @Override
+    public List<Apple2017Video> fetchVideos(Context context) {
+        Wrapper wrapper = parseJson(context, R.raw.tvos12, Wrapper.class);
+        return wrapper.assets;
+    }
+
+    private static class Wrapper {
+        private List<Apple2017Video> assets;
+    }
+}
