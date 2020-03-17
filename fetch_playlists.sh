f19ad591a4707889a86e695b91f967c879542bd5
Neil Turner <neilturner@gmail.com>
2020-03-17 14:56:37 +0000

    Fixed fetch script on macOS

--- fetch_playlists.sh
+++ fetch_playlists.sh
@@ -13,7 +13,7 @@ if [ $COUNT != 3 ]
 then
     wget http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json -O $RES_PATH/tvos10.json
     wget https://sylvan.apple.com/Aerials/2x/entries.json -O $RES_PATH/tvos11.json --no-check-certificate
-    wget https://sylvan.apple.com/Aerials/resources.tar -qO - --no-check-certificate | tar -xf - entries.json --to-stdout > $RES_PATH/tvos12.json
-    wget https://sylvan.apple.com/Aerials/resources-13.tar -qO - --no-check-certificate | tar -xf - entries.json --to-stdout > $RES_PATH/tvos13.json
+    wget https://sylvan.apple.com/Aerials/resources.tar -qO - --no-check-certificate | tar -xOf - entries.json > $RES_PATH/tvos12.json
+    wget https://sylvan.apple.com/Aerials/resources-13.tar -qO - --no-check-certificate | tar -xOf - entries.json > $RES_PATH/tvos13.json
 fi
 
