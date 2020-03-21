1be73af5ef4d34e3963a3257a5d920642022b2be
Neil Turner <neilturner@gmail.com>
2020-03-21 14:13:51 +0000

    No longer fetching tvOS10 feed

--- fetch_playlists.sh
+++ fetch_playlists.sh
@@ -11,7 +11,6 @@ COUNT=`ls -l $RES_PATH/tvos*.json | wc -l`
 # Skip download if files already exist
 if [ $COUNT != 1 ]
 then
-    wget http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json -O $RES_PATH/tvos10.json
     wget https://sylvan.apple.com/Aerials/resources-13.tar -qO - --no-check-certificate | tar -xOf - entries.json > $RES_PATH/tvos13.json
 fi
 
