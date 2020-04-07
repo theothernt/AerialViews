bb00b847f4e76c2b8ecbdeeeaea4078bd8ab2bad
Neil Turner <neilturner@gmail.com>
2020-04-07 15:10:01 +0100

    Cleaned up scripts

--- fetch_playlists.ps1
+++ fetch_playlists.ps1
@@ -13,13 +13,6 @@ $num = (Get-ChildItem -Path $path -Recurse -Filter "tvos*.json").Count
 # Skip download if files already exist
 if ($num -ne 1)
 {
-    #wget http://sylvan.apple.com/Aerials/resources-13.tar |  tar -xOf - entries.json
-    #(Invoke-WebRequest -URI http://sylvan.apple.com/Aerials/resources-13.tar).Content 
-    #curl.exe -s http://sylvan.apple.com/Aerials/resources-13.tar | tar -xOf - entries.json
-    
-    #curl.exe http://sylvan.apple.com/Aerials/resources-13.tar | tar -xOf - entries.json > $path/tvos13.json
-    #curl.exe -s http://sylvan.apple.com/Aerials/resources-13.tar | tar -xf - entries.json
-
     curl.exe http://sylvan.apple.com/Aerials/resources-13.tar -o resources-13.tar
     tar -xOf resources-13.tar entries.json > $path/tvos13.json
     Remove-Item resources-13.tar
--- fetch_playlists.sh
+++ fetch_playlists.sh
@@ -11,6 +11,7 @@ COUNT=`ls -l $RES_PATH/tvos*.json | wc -l`
 # Skip download if files already exist
 if [ $COUNT != 1 ]
 then
+    echo Downloading...
     wget https://sylvan.apple.com/Aerials/resources-13.tar -qO - --no-check-certificate | tar -xOf - entries.json > $RES_PATH/tvos13.json
 fi
 
