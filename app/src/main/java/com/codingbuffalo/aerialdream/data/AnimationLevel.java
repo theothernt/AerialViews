cddbe825b4f250e780697df18242bdb4b5493116
Neil Turner <neil@neilturner.com>
2020-06-02 21:22:38 +0100

    Added animation level definition

---
+++ app/src/main/java/com/codingbuffalo/aerialdream/data/AnimationLevel.java
@@ -0,0 +1,8 @@
+package com.codingbuffalo.aerialdream.data;
+
+public class AnimationLevel {
+    public static final int NONE        = 0;
+    public static final int SLOW        = 1;
+    public static final int FAST        = 2;
+    public static final int ADVANCED    = 3;
+}
