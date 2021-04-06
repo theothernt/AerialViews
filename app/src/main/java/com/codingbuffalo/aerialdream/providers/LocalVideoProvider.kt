9b24b80eb73a79426ea9880c162f1da787e51c76
Neil Turner <neil@neilturner.com>
2021-04-06 16:33:46 +0100

    Added initial Local provider

---
+++ app/src/main/java/com/codingbuffalo/aerialdream/providers/LocalVideoProvider.kt
@@ -0,0 +1,45 @@
+package com.codingbuffalo.aerialdream.providers
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.Uri
+import com.codingbuffalo.aerialdream.models.videos.AerialVideo
+import com.codingbuffalo.aerialdream.utils.FileHelper
+import java.util.*
+
+class LocalVideoProvider(context: Context) : VideoProvider(context) {
+
+    override fun fetchVideos(): List<AerialVideo> {
+        // get prefs
+        // Aerial folder or all
+
+        val videos = mutableListOf<AerialVideo>()
+        val filter = VideoFolder.AERIAL
+
+        val localVideos = FileHelper.findAllMedia(context)
+
+        for (video in localVideos) {
+
+            val containsFolder = video
+                    ?.toLowerCase(Locale.ROOT)
+                    ?.contains(filter.name.toLowerCase(Locale.ROOT))!!
+
+            if (filter != VideoFolder.ALL &&
+                    !containsFolder) {
+                continue
+            }
+
+            // use filename as location?
+            videos.add(AerialVideo(Uri.parse(video), ""))
+
+        }
+
+        //return videos
+        return emptyList()
+    }
+}
+
+enum class VideoFolder {
+    ALL,
+    AERIAL
+}
