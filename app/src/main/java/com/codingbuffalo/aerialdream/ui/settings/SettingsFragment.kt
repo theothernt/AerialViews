a2c028c975d5c43a60df335622df16fb59db2841
Neil Turner <neil@neilturner.com>
2021-05-09 22:02:05 +0100

    Fixed link to settings, improved option text

--- app/src/main/java/com/codingbuffalo/aerialdream/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/codingbuffalo/aerialdream/ui/settings/SettingsFragment.kt
@@ -1,15 +1,53 @@
 package com.codingbuffalo.aerialdream.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.codingbuffalo.aerialdream.R
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment :
+    PreferenceFragmentCompat(),
+    PreferenceManager.OnPreferenceTreeClickListener {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
     }
-}
 
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+
+        if(preference.key == null || !preference.key.contains("activate_screensaver"))
+            return super.onPreferenceTreeClick(preference)
+
+        // Check if the daydream intent is available - some devices (e.g. Nvidia Shield) do not support it
+        var intent = Intent(SCREENSAVER_SETTINGS)
+        if (!intentAvailable(intent)) {
+            // Try opening the daydream settings activity directly: https://gist.github.com/reines/bc798a2cb539f51877bb279125092104
+            intent = Intent(Intent.ACTION_MAIN).setClassName("com.android.tv.settings", "com.android.tv.settings.device.display.daydream.DaydreamActivity")
+            if (!intentAvailable(intent)) {
+                // If all else fails, open the normal settings screen
+                intent = Intent(SETTINGS)
+            }
+        }
+        startActivity(intent)
+        return true
+    }
+
+    private fun intentAvailable(intent: Intent): Boolean {
+        val manager = requireActivity().packageManager
+        val info = manager.queryIntentActivities(intent, 0)
+        return info.isNotEmpty()
+    }
+
+    companion object {
+        const val SETTINGS = "android.settings.SETTINGS"
+        const val SCREENSAVER_SETTINGS = "android.settings.DREAM_SETTINGS"
+        //const val VIDEO_SOURCE = "video_source"
+        //const val SOURCE_APPLE_2019 = "source_apple_2019"
+        //const val PERMISSION_READ_EXTERNAL_STORAGE = 1
+    }
+}
 //    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 //        setPreferencesFromResource(R.xml.settings, rootKey)
 //        findPreference("system_options").onPreferenceClickListener = this
@@ -17,20 +55,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
 //        //updateSummaries()
 //    }
 //
-//    override fun onPreferenceClick(preference: Preference): Boolean {
-//        // Check if the daydream intent is available - some devices (e.g. NVidia Shield) do not support it
-//        var intent = Intent(SCREENSAVER_SETTINGS)
-//        if (!intentAvailable(intent)) {
-//            // Try opening the daydream settings activity directly: https://gist.github.com/reines/bc798a2cb539f51877bb279125092104
-//            intent = Intent(Intent.ACTION_MAIN).setClassName("com.android.tv.settings", "com.android.tv.settings.device.display.daydream.DaydreamActivity")
-//            if (!intentAvailable(intent)) {
-//                // If all else fails, open the normal settings screen
-//                intent = Intent(SETTINGS)
-//            }
-//        }
-//        startActivity(intent)
-//        return true
-//    }
+
 //
 //    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
 //        if (key == VIDEO_SOURCE) {
@@ -77,30 +102,3 @@ class SettingsFragment : PreferenceFragmentCompat() {
 //        listPrefs = findPreference(VIDEO_SOURCE) as ListPreference
 //        listPrefs.summary = listPrefs.entry
 //    }
-//
-//    private fun intentAvailable(intent: Intent): Boolean {
-//        val manager = activity!!.packageManager
-//        val info = manager.queryIntentActivities(intent, 0)
-//        return info.isNotEmpty()
-//    }
-//
-//    class AppleVideosSettingsFragment : PreferenceFragmentCompat() {
-//        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-//            setPreferencesFromResource(R.xml.settings_apple_videos, rootKey)
-//        }
-//    }
-//
-//    class LocalVideosSettingsFragment : PreferenceFragmentCompat() {
-//        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-//            setPreferencesFromResource(R.xml.settings_local_videos, rootKey)
-//        }
-//    }
-//
-//    companion object {
-//        const val SETTINGS = "android.settings.SETTINGS"
-//        const val SCREENSAVER_SETTINGS = "android.settings.DREAM_SETTINGS"
-//        const val VIDEO_SOURCE = "video_source"
-//        const val SOURCE_APPLE_2019 = "source_apple_2019"
-//        const val PERMISSION_READ_EXTERNAL_STORAGE = 1
-//    }
-//}
