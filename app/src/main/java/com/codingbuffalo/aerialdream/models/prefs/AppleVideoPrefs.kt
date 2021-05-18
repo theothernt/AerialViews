fd811789ad2185fdeac928e936ece151e1090f4b
Neil Turner <neil@neilturner.com>
2021-05-18 16:05:16 +0100

    Added descriptions to some prefs

--- app/src/main/java/com/codingbuffalo/aerialdream/models/prefs/AppleVideoPrefs.kt
+++ app/src/main/java/com/codingbuffalo/aerialdream/models/prefs/AppleVideoPrefs.kt
@@ -8,7 +8,7 @@ import com.codingbuffalo.aerialdream.models.AppleVideoLocation
 object AppleVideoPrefs : KotprefModel() {
     override val kotprefName = "${context.packageName}_preferences"
 
-    var enabled: Boolean by booleanPref(true, "apple_videos_enabled")
+    var enabled by booleanPref(true, "apple_videos_enabled")
     var quality by enumValuePref(AppleVideoQuality.VIDEO_1080_SDR, "apple_videos_quality")
     var location by enumValuePref(AppleVideoLocation.REMOTE, "apple_videos_location")
 }
--- app/src/main/java/com/codingbuffalo/aerialdream/models/prefs/GeneralPrefs.kt
+++ app/src/main/java/com/codingbuffalo/aerialdream/models/prefs/GeneralPrefs.kt
@@ -5,15 +5,15 @@ import com.chibatching.kotpref.KotprefModel
 object GeneralPrefs : KotprefModel() {
     override val kotprefName = "${context.packageName}_preferences"
 
-    var showLocation: Boolean by booleanPref(true, "show_location")
-    var showClock: Boolean by booleanPref(true, "show_clock")
-    var alternateTextPosition: Boolean by booleanPref(false, "alt_text_position")
+    var showLocation by booleanPref(true, "show_location")
+    var showClock by booleanPref(true, "show_clock")
+    var alternateTextPosition by booleanPref(false, "alt_text_position")
 
-    var muteVideos: Boolean by booleanPref(true, "mute_videos")
-    var shuffleVideos: Boolean by booleanPref(true, "shuffle_videos")
-    var removeDuplicates: Boolean by booleanPref(true, "remove_duplicates")
+    var muteVideos by booleanPref(true, "mute_videos")
+    var shuffleVideos by booleanPref(true, "shuffle_videos")
+    var removeDuplicates by booleanPref(true, "remove_duplicates")
 
-    var enableTunneling: Boolean by booleanPref(true, "enable_tunneling")
-    var reducedBuffers: Boolean by booleanPref(false, "reduced_buffers")
-    var exceedRenderer: Boolean by booleanPref(false, "exceed_renderer")
+    var enableTunneling by booleanPref(true, "enable_tunneling")
+    var reducedBuffers by booleanPref(false, "reduced_buffers")
+    var exceedRenderer by booleanPref(false, "exceed_renderer")
 }
