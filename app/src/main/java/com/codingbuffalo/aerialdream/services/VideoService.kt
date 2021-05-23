90c651d06dc9ac23faf842aa440c67ceb3d1eb5c
Neil Turner <neilturner@gmail.com>
2021-05-23 19:00:33 +0100

    Fixed duplicate removal
    Added more logging.

--- app/src/main/java/com/codingbuffalo/aerialdream/services/VideoService.kt
+++ app/src/main/java/com/codingbuffalo/aerialdream/services/VideoService.kt
@@ -2,6 +2,7 @@ package com.codingbuffalo.aerialdream.services
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
 import com.codingbuffalo.aerialdream.models.VideoPlaylist
 import com.codingbuffalo.aerialdream.models.prefs.AppleVideoPrefs
 import com.codingbuffalo.aerialdream.models.prefs.GeneralPrefs
@@ -12,7 +13,6 @@ import com.codingbuffalo.aerialdream.providers.LocalVideoProvider
 import com.codingbuffalo.aerialdream.providers.VideoProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.util.*
 
 class VideoService(context: Context) {
     private val providers = mutableListOf<VideoProvider>()
@@ -26,27 +26,37 @@ class VideoService(context: Context) {
     }
 
     suspend fun fetchVideos(): VideoPlaylist = withContext(Dispatchers.IO) {
-        val videos = mutableListOf<AerialVideo>()
+        var videos = mutableListOf<AerialVideo>()
 
         providers.forEach {
             videos.addAll(it.fetchVideos())
         }
 
         if (videos.isEmpty()) {
+            Log.i(TAG, "No videos, adding empty one")
             videos.add(AerialVideo(Uri.parse(""), ""))
         }
 
         if (GeneralPrefs.removeDuplicates) {
+            var numVideos = videos.size
             // Remove duplicates based on full path
-            videos.distinctBy { it.uri.toString().lowercase() }
+            videos = videos.distinctBy { it.uri.toString().lowercase() } as MutableList<AerialVideo>
+            Log.i(TAG, "Vids removed based on full path: ${numVideos - videos.size}")
 
+            numVideos = videos.size
             // Remove duplicates based on filename only
-            videos.distinctBy { it.uri.lastPathSegment?.lowercase() }
+            videos = videos.distinctBy { it.uri.lastPathSegment?.lowercase() } as MutableList<AerialVideo>
+            Log.i(TAG, "Vids removed based on filename: ${numVideos - videos.size}")
         }
 
         if (GeneralPrefs.shuffleVideos)
             videos.shuffle()
 
+        Log.i(TAG, "Total vids: ${videos.size}")
         VideoPlaylist(videos)
+    }
+
+    companion object {
+        private const val TAG = "VideoService"
     }
 }
