dda7a955b4cbe3bb0d829cabc2cf57154f0a3738
Neil Turner <neil@neilturner.com>
2021-08-04 16:10:19 +0100

    Capabilities are now displayed
    Needs more testing and tweaking.

--- app/src/main/java/com/codingbuffalo/aerialdream/ui/settings/CapabilitiesFragment.kt
+++ app/src/main/java/com/codingbuffalo/aerialdream/ui/settings/CapabilitiesFragment.kt
@@ -1,11 +1,57 @@
 package com.codingbuffalo.aerialdream.ui.settings
 
 import android.os.Bundle
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.codingbuffalo.aerialdream.R
+import com.codingbuffalo.aerialdream.services.getCodecs
+import com.codingbuffalo.aerialdream.services.getDisplays
 
 class CapabilitiesFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_capabilities, rootKey)
+        updateCapabilities()
+    }
+
+    private fun updateCapabilities()
+    {
+        val display = findPreference<ListPreference>("capabilities_display") as Preference
+        val resolution = findPreference<ListPreference>("capabilities_resolution") as Preference
+        val codecs = findPreference<ListPreference>("capabilities_codecs") as Preference
+
+        display.summary = buildDisplaySummary()
+        resolution.summary = buildResolutionSummary()
+        codecs.summary = buildCodecsSummary()
+    }
+
+    private fun buildDisplaySummary(): String {
+        val display = getDisplays(activity).first()
+        var summary = "Supports HDR: ${display.supportsHDR}"
+        if (display.supportsHDR) {
+            summary += "\nHDR Formats: ${display.hdrFormats.joinToString(", ")}"
+        }
+        return summary
+    }
+
+    private fun buildResolutionSummary(): String {
+        val display = getDisplays(activity).first()
+        var summary = "Render Output: ${display.renderOutput}"
+        if (display.physicalOutput != null) {
+            summary += "\nPhysical Output: ${display.physicalOutput}"
+            //summary += "\nActive Display Mode: ${display.physicalOutput.id}"
+        }
+        return summary
+    }
+
+    private fun buildCodecsSummary(): String {
+        var summary = ""
+        getCodecs().forEach{ codec ->
+            if (codec.name.contains("hevc") || codec.name.contains("dolby")) {
+                summary += "${codec.name}\n"
+                //summary += "\n${codec.mimeTypes.joinToString(", ")}"
+            }
+        }
+        return summary
     }
 }
