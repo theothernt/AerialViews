c0fd211f8fa122bb3c09440a9154f08b0579fdd8
Neil Turner <neil@neilturner.com>
2021-10-04 15:46:46 +0100

    Capabilities screen should be easier to understand

--- app/src/main/java/com/neilturner/aerialviews/ui/settings/CapabilitiesFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/CapabilitiesFragment.kt
@@ -6,6 +6,7 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.neilturner.aerialviews.R
 import com.neilturner.aerialviews.services.CodecType
+import com.neilturner.aerialviews.services.HDRFormat
 import com.neilturner.aerialviews.services.getCodecs
 import com.neilturner.aerialviews.services.getDisplay
 
@@ -22,16 +23,26 @@ class CapabilitiesFragment : PreferenceFragmentCompat() {
         val codecs = findPreference<ListPreference>("capabilities_codecs") as Preference
 
         display.summary = buildDisplaySummary()
-        resolution.summary = buildResolutionSummary()
         codecs.summary = buildCodecsSummary()
+        resolution.summary = buildResolutionSummary()
     }
 
     private fun buildDisplaySummary(): String {
+        var summary = ""
+        var supportsHDR10 = "False"
+        var supportsDolbyVision = "False"
+
         val display = getDisplay(activity)
-        var summary = "Supports HDR: ${display.supportsHDR}"
         if (display.supportsHDR && display.hdrFormats.isNotEmpty()) {
-            summary += "\nHDR Formats: ${display.hdrFormats.joinToString(", ")}"
+            if (display.hdrFormats.contains(HDRFormat.DOLBY_VISION))
+                supportsDolbyVision = "True"
+            if (display.hdrFormats.contains(HDRFormat.HDR10))
+                supportsHDR10 = "True"
         }
+
+        summary += "Supports HDR10: $supportsHDR10\n"
+        summary += "Supports Dolby Vision: $supportsDolbyVision\n"
+
         return summary
     }
 
@@ -40,20 +51,27 @@ class CapabilitiesFragment : PreferenceFragmentCompat() {
         var summary = "Render Output: ${display.renderOutput}"
         if (display.physicalOutput != null) {
             summary += "\nPhysical Output: ${display.physicalOutput}"
-            //summary += "\nActive Display Mode: ${display.physicalOutput.id}"
         }
         return summary
     }
 
     private fun buildCodecsSummary(): String {
         var summary = ""
+        val supportsAVC = "True"
+        var supportsHEVC = "False"
+        var supportsDolbyVision = "False"
+
         getCodecs().forEach{ codec ->
-            if ((codec.name.contains("hevc") || codec.name.contains("dolby")) &&
-                    codec.codingFunction == CodecType.DECODER) {
-                summary += "${codec.name}\n"
-                //summary += "\n${codec.mimeTypes.joinToString(", ")}"
-            }
+            if (codec.name.lowercase().contains("hevc") && codec.codingFunction == CodecType.DECODER)
+                supportsHEVC = "True"
+
+            if (codec.name.lowercase().contains("dolby") && codec.codingFunction == CodecType.DECODER)
+                supportsDolbyVision = "True"
         }
+
+        summary += "Supports AVC: $supportsAVC\n"
+        summary += "Supports HEVC: $supportsHEVC\n"
+        summary += "Supports Dolby Vision: $supportsDolbyVision\n"
         return summary
     }
 }
