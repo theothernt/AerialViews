ca6dab71bb5a8dafd69a00cd250ba072979a408a
Neil Turner <neil@neilturner.com>
2022-01-24 15:45:15 +0000

    Updated location style pref to show current value

--- app/src/main/java/com/neilturner/aerialviews/ui/settings/InterfaceFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/InterfaceFragment.kt
@@ -2,12 +2,39 @@
 
 package com.neilturner.aerialviews.ui.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.preference.ListPreference
 import com.neilturner.aerialviews.R
 import androidx.preference.PreferenceFragmentCompat
+import java.util.*
 
-class InterfaceFragment : PreferenceFragmentCompat() {
+class InterfaceFragment :
+    PreferenceFragmentCompat(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_interface, rootKey)
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        updateSummaries()
+    }
+
+    override fun onDestroy() {
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        super.onDestroy()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        updateSummaries()
+    }
+
+    private fun updateSummaries() {
+        val locationStyle = findPreference<ListPreference>("show_location_style") as ListPreference
+        val locationStyleTitle = context?.getString(R.string.interface_show_location_style_title)
+
+        val formattedLocationStyle = locationStyle.entry.toString()
+            .replaceFirstChar { it.lowercase(Locale.getDefault()) }
+
+        locationStyle.title = "$locationStyleTitle - $formattedLocationStyle"
     }
 }
