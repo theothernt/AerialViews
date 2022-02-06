9ddc2fc3dfe78d7901ed3b1b5239b824f33a3061
Neil Turner <neil@neilturner.com>
2022-02-06 19:12:09 +0000

    Added prefs + UI for more network settings

--- app/src/main/java/com/neilturner/aerialviews/models/prefs/NetworkVideoPrefs.kt
+++ app/src/main/java/com/neilturner/aerialviews/models/prefs/NetworkVideoPrefs.kt
@@ -1,6 +1,8 @@
 package com.neilturner.aerialviews.models.prefs
 
 import com.chibatching.kotpref.KotprefModel
+import com.neilturner.aerialviews.R
+import java.util.*
 
 object NetworkVideoPrefs : KotprefModel() {
     override val kotprefName = "${context.packageName}_preferences"
@@ -11,4 +13,16 @@ object NetworkVideoPrefs : KotprefModel() {
     var hostName by stringPref("", "network_videos_hostname")
     var shareName by stringPref("", "network_videos_sharename")
     var domainName by stringPref("WORKGROUP", "network_videos_domainname")
+
+    var enableEncryption by booleanPref(false, "network_videos_enable_encryption")
+
+    val smbDialects by stringSetPref("network_videos_smb_dialects") {
+        val smbDialects = context.resources.getStringArray(R.array.network_videos_smb_dialects_default)
+        val set = TreeSet<String>()
+        set.addAll(smbDialects)
+        set
+        // Must be sorted to get correct order for use in SmbConfig.withDialects(x,y,z)
+        // prefs.smbDialects.sortedByDescending { it }.joinToString(",")
+    }
+
 }
