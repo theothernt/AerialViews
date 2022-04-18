46f554837656b8ab494aa2e072227b994c143056
Neil Turner <neil@neilturner.com>
2022-04-18 21:14:30 +0100

    Updated unit testing setup

---
+++ app/src/test/java/com/neilturner/aerialviews/Test.kt
@@ -0,0 +1,14 @@
+package com.neilturner.aerialviews.Test
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Test {
+
+    @Test
+    fun addition() {
+
+        val answer = 2 + 2
+        assertEquals(4, answer)
+    }
+}
