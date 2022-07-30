a362814b820e7dda6056be5433f141b39df9f212
Neil Turner <neil@neilturner.com>
2022-07-30 16:09:38 +0100

    Added device detection code
    For Google TV, Fire TV, and Nvidia Shield.

---
+++ app/src/main/java/com/neilturner/aerialviews/utils/DeviceHelper.kt
@@ -0,0 +1,40 @@
+package com.neilturner.aerialviews.utils
+
+import android.os.Build
+import java.util.*
+
+object DeviceHelper {
+
+    // Based on
+    // https://stackoverflow.com/a/27836910/247257
+
+    fun getDeviceName(): String {
+        val manufacturer = Build.MANUFACTURER
+        val model = Build.MODEL
+        return if (model.lowercase(Locale.getDefault())
+                .startsWith(manufacturer.lowercase(Locale.getDefault()))
+        ) {
+            capitalize(model)
+        } else {
+            capitalize(manufacturer) + " " + model
+        }
+    }
+
+    private fun capitalize(s: String): String {
+        if (s.isEmpty()) {
+            return ""
+        }
+        val first = s[0]
+        return if (Character.isUpperCase(first)) {
+            s
+        } else {
+            first.uppercaseChar().toString() + s.substring(1)
+        }
+    }
+
+    fun isFireTV(): Boolean = getDeviceName().contains("AFT", true)
+
+    fun isNvidaShield(): Boolean = getDeviceName().contains("NVIDIA", true)
+
+    fun isChromecastWithGoogleTV(): Boolean = getDeviceName().contains("Google Chromecast", true)
+}
