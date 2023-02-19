c80e472cfd58e02ba3605dc76986a97c5618490f
Neil Turner <neil@neilturner.com>
2023-02-19 17:16:06 +0000

    Migrate video quality setting
    Added more logging too

--- app/src/main/java/com/neilturner/aerialviews/utils/MigrationHelper.kt
+++ app/src/main/java/com/neilturner/aerialviews/utils/MigrationHelper.kt
@@ -13,6 +13,8 @@ class MigrationHelper(val context: Context) {
         val latestVersion = BuildConfig.VERSION_CODE
         val lastKnownVersion = getLastKnownVersion()
 
+        Log.i(TAG, "Build code $lastKnownVersion, Last known version $lastKnownVersion")
+
         // If first install, exit early
         if (PackageHelper.isFirstInstall(context)) {
             Log.i(TAG, "Fresh install, no migration needed")
@@ -34,7 +36,7 @@ class MigrationHelper(val context: Context) {
         }
 
         if (lastKnownVersion <= 10) release10()
-        if (lastKnownVersion <= 11) release11()
+        //if (lastKnownVersion <= 11) release11()
 
         // After all migrations, set version to latest
         updateKnownVersion(latestVersion)
@@ -42,10 +44,20 @@ class MigrationHelper(val context: Context) {
 
     private fun release10() {
         Log.i(TAG, "Migrating settings for release 10")
+
         val appleVideosEnabled = prefs.getBoolean("apple_videos_enabled", false)
         if (!appleVideosEnabled) {
+            Log.i(TAG, "Disabling new community videos")
             prefs.edit().putBoolean("comm1_videos_enabled", false).apply()
             prefs.edit().putBoolean("comm2_videos_enabled", false).apply()
+            return
+        }
+
+        val videoQuality = prefs.getString("apple_videos_quality", "").toStringOrEmpty()
+        if (videoQuality.contains("4K", true)) {
+            Log.i(TAG, "Setting community videos to 4K")
+            prefs.edit().putString("comm1_videos_quality", "VIDEO_4K_SDR").apply()
+            prefs.edit().putString("comm2_videos_quality", "VIDEO_4K_SDR").apply()
         }
     }
 
@@ -60,6 +72,7 @@ class MigrationHelper(val context: Context) {
 
     // Update saved revision code or return 0
     private fun updateKnownVersion(versionCode: Int) {
+        Log.i(TAG, "Updating last known version to $versionCode")
         prefs.edit().putInt("last_known_version", versionCode).apply()
     }
 
