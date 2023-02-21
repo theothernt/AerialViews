8061d58432cec93c36f30d6157c1e17c52b5b959
Neil Turner <neil@neilturner.com>
2023-02-21 17:58:47 +0000

    Default to H.264 in emulator and old FireTV
    Changes default prefs but not UI as it's a hard coded value.

--- app/src/main/java/com/neilturner/aerialviews/models/prefs/Comm2VideoPrefs.kt
+++ app/src/main/java/com/neilturner/aerialviews/models/prefs/Comm2VideoPrefs.kt
@@ -3,10 +3,11 @@ package com.neilturner.aerialviews.models.prefs
 import com.chibatching.kotpref.KotprefModel
 import com.chibatching.kotpref.enumpref.enumValuePref
 import com.neilturner.aerialviews.models.VideoQuality
+import com.neilturner.aerialviews.utils.DeviceHelper
 
 object Comm2VideoPrefs : KotprefModel() {
     override val kotprefName = "${context.packageName}_preferences"
 
     var enabled by booleanPref(true, "comm2_videos_enabled")
-    var quality by enumValuePref(VideoQuality.VIDEO_1080_SDR, "comm2_videos_quality")
+    var quality by enumValuePref(if (DeviceHelper.hasHevcSupport()) VideoQuality.VIDEO_1080_SDR else VideoQuality.VIDEO_1080_H264, "comm2_videos_quality")
 }
