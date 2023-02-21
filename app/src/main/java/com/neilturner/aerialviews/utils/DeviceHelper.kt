689887bed3c9ca58cb8fb49819398692ffd77bc1
Neil Turner <neil@neilturner.com>
2023-02-21 17:21:35 +0000

    Updated emulator detection

--- app/src/main/java/com/neilturner/aerialviews/utils/DeviceHelper.kt
+++ app/src/main/java/com/neilturner/aerialviews/utils/DeviceHelper.kt
@@ -36,8 +36,24 @@ object DeviceHelper {
         }
     }
 
-    // Simple but it should work for the moment!
-    fun isEmulator(): Boolean = deviceName().contains("sdk_google_atv", true)
+    // https://stackoverflow.com/a/55355049/247257
+    fun isEmulator(): Boolean = (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")) ||
+        Build.FINGERPRINT.startsWith("generic") ||
+        Build.FINGERPRINT.startsWith("unknown") ||
+        Build.HARDWARE.contains("goldfish") ||
+        Build.HARDWARE.contains("ranchu") ||
+        Build.MODEL.contains("google_sdk") ||
+        Build.MODEL.contains("Emulator") ||
+        Build.MODEL.contains("Android SDK built for x86") ||
+        Build.MANUFACTURER.contains("Genymotion") ||
+        Build.PRODUCT.contains("sdk_google") ||
+        Build.PRODUCT.contains("google_sdk") ||
+        Build.PRODUCT.contains("sdk") ||
+        Build.PRODUCT.contains("sdk_x86") ||
+        Build.PRODUCT.contains("sdk_gphone64_arm64") ||
+        Build.PRODUCT.contains("vbox86p") ||
+        Build.PRODUCT.contains("emulator") ||
+        Build.PRODUCT.contains("simulator")
 
     fun isFireTV(): Boolean = deviceName().contains("AFT", true)
 
