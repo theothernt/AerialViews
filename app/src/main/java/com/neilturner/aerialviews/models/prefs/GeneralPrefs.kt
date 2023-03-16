e821e390359574a0242795b2b68b99627623461b
Neil Turner <neil@neilturner.com>
2023-03-16 14:05:09 +0000

    Add UI for new pref

--- app/src/main/java/com/neilturner/aerialviews/models/prefs/GeneralPrefs.kt
+++ app/src/main/java/com/neilturner/aerialviews/models/prefs/GeneralPrefs.kt
@@ -16,6 +16,7 @@ object GeneralPrefs : KotprefModel() {
     var enableTunneling by booleanPref(true, "enable_tunneling")
     var exceedRenderer by booleanPref(false, "exceed_renderer")
     var bufferingStrategy by stringPref("DEFAULT", "performance_buffering_strategy")
+    var philipsDolbyVisionFix by booleanPref(false, "philips_dolby_vision_fix")
 
     var filenameAsLocation by booleanPref(true, "any_videos_filename_location")
     var useAppleManifests by booleanPref(true, "any_videos_use_apple_manifests")
