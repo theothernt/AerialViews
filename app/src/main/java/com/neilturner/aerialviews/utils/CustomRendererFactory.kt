35e013d39e652a2710574f75d99600f1f46c98b1
Neil Turner <neil@neilturner.com>
2023-03-16 13:15:34 +0000

    Some refactoring

--- app/src/main/java/com/neilturner/aerialviews/utils/CustomRendererFactory.kt
+++ app/src/main/java/com/neilturner/aerialviews/utils/CustomRendererFactory.kt
@@ -8,20 +8,19 @@ import com.google.android.exoplayer2.mediacodec.MediaCodecAdapter
 
 // https://github.com/bdwixx/AerialViews/blob/philips-exoplayer-fix/app/src/main/java/com/neilturner/aerialviews/utils/CustomRendererFactory.java
 
-class CustomRendererFactory(context: Context?) :
-    DefaultRenderersFactory(context!!) {
-    private var pmcaf: PhilipsMediaCodecAdapterFactory? = null
+class CustomRendererFactory(context: Context?) : DefaultRenderersFactory(context!!) {
+    private var renderFactory: PhilipsMediaCodecAdapterFactory? = null
     override fun getCodecAdapterFactory(): MediaCodecAdapter.Factory {
         Log.d(TAG, "Manufacturer: " + Build.MANUFACTURER)
         if (Build.MANUFACTURER != "Philips") {
             Log.d(TAG, "Using default MediaCodecAdapter")
             return super.getCodecAdapterFactory()
         }
-        if (pmcaf == null) {
-            pmcaf = PhilipsMediaCodecAdapterFactory()
+        if (renderFactory == null) {
+            renderFactory = PhilipsMediaCodecAdapterFactory()
         }
-        Log.d(TAG, "Using custom/philips MediaCodecAdapter")
-        return pmcaf as PhilipsMediaCodecAdapterFactory
+        Log.d(TAG, "Using Custom/Philips MediaCodecAdapter")
+        return renderFactory as PhilipsMediaCodecAdapterFactory
     }
 
     companion object {
--- app/src/main/java/com/neilturner/aerialviews/utils/PhilipsMediaCodecAdapterFactory.kt
+++ app/src/main/java/com/neilturner/aerialviews/utils/PhilipsMediaCodecAdapterFactory.kt
@@ -1,7 +1,6 @@
 package com.neilturner.aerialviews.utils
 
 import android.media.MediaExtractor
-import android.util.Log
 import com.google.android.exoplayer2.mediacodec.MediaCodecAdapter
 import com.google.android.exoplayer2.mediacodec.SynchronousMediaCodecAdapter
 import java.io.IOException
@@ -18,38 +17,28 @@ class PhilipsMediaCodecAdapterFactory : MediaCodecAdapter.Factory {
             return SynchronousMediaCodecAdapter.Factory().createAdapter(configuration)
         }
         try {
-            val me = MediaExtractor()
-            me.setDataSource(mediaUri!!)
+            val extractor = MediaExtractor()
+            extractor.setDataSource(mediaUri!!)
             mediaUri = null
-            val tf = me.getTrackFormat(0)
-            val csd0 = tf.getByteBuffer("csd-0")
-            if (csd0 != null && csd0.limit() != 0) {
-                configuration.mediaFormat.setByteBuffer("csd-0", csd0)
-                Log.d(TAG, "csd-0 set")
-
-                // TODO: Remove this, its for testing only
-                val sb = StringBuilder()
-                while (csd0.hasRemaining()) {
-                    val b = csd0.get()
-                    sb.append(String.format("%02x", b.toInt() and 0xff))
-                }
-                Log.d(TAG, "csd-0: $sb")
+            val trackFormat = extractor.getTrackFormat(0)
+            val codecData = trackFormat.getByteBuffer("csd-0")
+            if (codecData != null && codecData.limit() != 0) {
+                configuration.mediaFormat.setByteBuffer("csd-0", codecData)
                 // After reading, position is at end and the codec starts
                 // reading at current pos resulting in failed playback
                 // so we need to set position manually
-                csd0.position(0)
+                codecData.position(0)
             }
         } catch (e: IOException) {
             throw RuntimeException(e)
         } finally {
             mediaUri = null
         }
-        Log.d(TAG, "configuration.format: " + configuration.format)
         return SynchronousMediaCodecAdapter.Factory().createAdapter(configuration)
     }
 
     companion object {
-        private const val TAG = "PMCodecAdapterFactory"
+        private const val TAG = "PhilipsMediaCodecAdapter"
         var mediaUri: String? = null
     }
 }
