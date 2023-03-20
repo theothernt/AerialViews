64345ba7c688314edc8905a4af5074a6666803ad
Neil Turner <neil@neilturner.com>
2023-03-20 15:46:33 +0000

    Removed device check from Philips fix
    If people want to try the 'fix' on other devices, they can.

--- app/src/main/java/com/neilturner/aerialviews/utils/CustomRendererFactory.kt
+++ app/src/main/java/com/neilturner/aerialviews/utils/CustomRendererFactory.kt
@@ -9,18 +9,9 @@ import com.google.android.exoplayer2.mediacodec.MediaCodecAdapter
 // https://github.com/bdwixx/AerialViews/blob/philips-exoplayer-fix/app/src/main/java/com/neilturner/aerialviews/utils/CustomRendererFactory.java
 
 class CustomRendererFactory(context: Context) : DefaultRenderersFactory(context) {
-    private var renderFactory: PhilipsMediaCodecAdapterFactory? = null
     override fun getCodecAdapterFactory(): MediaCodecAdapter.Factory {
-        Log.d(TAG, "Manufacturer: " + Build.MANUFACTURER)
-        if (Build.MANUFACTURER != "Philips") {
-            Log.d(TAG, "Using default MediaCodecAdapter")
-            return super.getCodecAdapterFactory()
-        }
-        if (renderFactory == null) {
-            renderFactory = PhilipsMediaCodecAdapterFactory()
-        }
         Log.d(TAG, "Using Custom/Philips MediaCodecAdapter")
-        return renderFactory as PhilipsMediaCodecAdapterFactory
+        return PhilipsMediaCodecAdapterFactory()
     }
 
     companion object {
