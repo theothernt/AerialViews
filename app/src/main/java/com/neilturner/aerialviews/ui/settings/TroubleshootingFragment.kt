0a18abdf3d5385fd93517cbe99b65ec38951114c
Neil Turner <neil@neilturner.com>
2023-03-25 21:54:54 +0000

    Added summary to multi select list

--- app/src/main/java/com/neilturner/aerialviews/ui/settings/TroubleshootingFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/TroubleshootingFragment.kt
@@ -3,13 +3,25 @@
 package com.neilturner.aerialviews.ui.settings
 
 import android.os.Bundle
+import androidx.preference.MultiSelectListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.neilturner.aerialviews.R
-import java.util.*
+import com.neilturner.aerialviews.utils.setSummaryFromValues
 
 class TroubleshootingFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_troubleshooting, rootKey)
+        updateSummary()
+    }
+
+    private fun updateSummary() {
+        val dialects = findPreference<MultiSelectListPreference>("network_videos_smb_dialects")
+        dialects?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
+            dialects?.setSummaryFromValues(newValue as Set<String>)
+            true
+        }
+        dialects?.setSummaryFromValues(dialects.values)
     }
 }
--- app/src/main/java/com/neilturner/aerialviews/utils/Extensions.kt
+++ app/src/main/java/com/neilturner/aerialviews/utils/Extensions.kt
@@ -3,6 +3,7 @@ package com.neilturner.aerialviews.utils
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.os.Build
+import androidx.preference.MultiSelectListPreference
 
 // https://stackoverflow.com/a/36795003/247257
 fun Any?.toStringOrEmpty() = this?.toString() ?: ""
@@ -15,3 +16,8 @@ fun PackageManager.getPackageInfoCompat(packageName: String, flags: Int = 0): Pa
         @Suppress("DEPRECATION")
         getPackageInfo(packageName, flags)
     }
+
+// https://stackoverflow.com/a/53428355/247257
+fun MultiSelectListPreference.setSummaryFromValues(values: Set<String>) {
+    summary = values.joinToString(", ") { entries[findIndexOfValue(it)] }
+}
