f950aae14da7387fcc3441feb1d8aafe7a1ead9e
Neil Turner <neil@neilturner.com>
2023-04-18 15:22:12 +0100

    Updated data usage summary code

--- app/src/main/java/com/neilturner/aerialviews/ui/sources/AppleVideosFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/sources/AppleVideosFragment.kt
@@ -2,39 +2,33 @@
 
 package com.neilturner.aerialviews.ui.sources
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.neilturner.aerialviews.R
 
-class AppleVideosFragment :
-    PreferenceFragmentCompat(),
-    SharedPreferences.OnSharedPreferenceChangeListener {
+class AppleVideosFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.sources_apple_videos, rootKey)
-        preferenceManager.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
-        updateSummaries()
+        updateSummary()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        preferenceManager.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-        updateSummaries()
-    }
-
-    private fun updateSummaries() {
-        val res = context?.resources!!
+    private fun updateSummary() {
         val quality = findPreference<ListPreference>("apple_videos_quality")
+        quality?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
+            quality?.findIndexOfValue(newValue as String)?.let { updateDataUsageSummary(it) }
+            true
+        }
+        quality?.findIndexOfValue(quality.value)?.let { updateDataUsageSummary(it) }
+    }
+
+    private fun updateDataUsageSummary(index: Int) {
+        val res = context?.resources!!
         val dataUsage = findPreference<Preference>("apple_videos_data_usage")
-        val index = quality?.findIndexOfValue(quality.value)
-        val bitrates = res.getStringArray(R.array.apple_videos_data_usage_values)
-        val bitrate = index?.let { bitrates[it] }
+        val bitrateList = res.getStringArray(R.array.apple_videos_data_usage_values)
+        val bitrate = bitrateList[index]
         dataUsage?.summary = String.format(res.getString(R.string.apple_videos_data_estimate_summary), bitrate)
     }
 }
