03d5cccf729913165beafdd32a2eefc4e82ab0e1
Neil Turner <neil@neilturner.com>
2023-04-22 16:16:17 +0100

    Updated video providers to use localised strings

--- app/src/main/java/com/neilturner/aerialviews/providers/Comm1VideoProvider.kt
+++ app/src/main/java/com/neilturner/aerialviews/providers/Comm1VideoProvider.kt
@@ -7,19 +7,23 @@ import com.neilturner.aerialviews.models.prefs.Comm1VideoPrefs
 import com.neilturner.aerialviews.models.videos.AerialVideo
 import com.neilturner.aerialviews.utils.JsonHelper
 import com.neilturner.aerialviews.utils.JsonHelper.parseJson
+import com.neilturner.aerialviews.utils.JsonHelper.parseJsonMap
 
 class Comm1VideoProvider(context: Context, private val prefs: Comm1VideoPrefs) : VideoProvider(context) {
 
     override fun fetchVideos(): List<AerialVideo> {
         val quality = prefs.quality
         val videos = mutableListOf<AerialVideo>()
+        val strings = parseJsonMap(context, R.raw.comm1_strings)
         val wrapper = parseJson(context, R.raw.comm1, JsonHelper.Comm1Videos::class.java)
         wrapper.assets?.forEach {
             videos.add(
                 AerialVideo(
                     it.uri(quality)!!,
                     it.location,
-                    it.pointsOfInterest
+                    it.pointsOfInterest.mapValues { poi ->
+                        strings[poi.value] ?: it.location
+                    }
                 )
             )
         }
--- app/src/main/java/com/neilturner/aerialviews/providers/Comm2VideoProvider.kt
+++ app/src/main/java/com/neilturner/aerialviews/providers/Comm2VideoProvider.kt
@@ -7,19 +7,23 @@ import com.neilturner.aerialviews.models.prefs.Comm2VideoPrefs
 import com.neilturner.aerialviews.models.videos.AerialVideo
 import com.neilturner.aerialviews.utils.JsonHelper
 import com.neilturner.aerialviews.utils.JsonHelper.parseJson
+import com.neilturner.aerialviews.utils.JsonHelper.parseJsonMap
 
 class Comm2VideoProvider(context: Context, private val prefs: Comm2VideoPrefs) : VideoProvider(context) {
 
     override fun fetchVideos(): List<AerialVideo> {
         val quality = prefs.quality
         val videos = mutableListOf<AerialVideo>()
+        val strings = parseJsonMap(context, R.raw.comm2_strings)
         val wrapper = parseJson(context, R.raw.comm2, JsonHelper.Comm2Videos::class.java)
         wrapper.assets?.forEach {
             videos.add(
                 AerialVideo(
                     it.uri(quality)!!,
                     it.location,
-                    it.pointsOfInterest
+                    it.pointsOfInterest.mapValues { poi ->
+                        strings[poi.value] ?: it.location
+                    }
                 )
             )
         }
