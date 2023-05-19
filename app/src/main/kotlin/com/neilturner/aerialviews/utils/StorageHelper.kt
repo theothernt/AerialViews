21c09f00319b40eaa66e7ee2dddfd78d6480f12b
Neil Turner <neil@neilturner.com>
2023-05-19 16:54:16 +0100

    Added more helpers

--- app/src/main/kotlin/com/neilturner/aerialviews/utils/StorageHelper.kt
+++ app/src/main/kotlin/com/neilturner/aerialviews/utils/StorageHelper.kt
@@ -7,13 +7,96 @@ import android.os.storage.StorageManager
 import android.os.storage.StorageVolume
 import android.util.Log
 import androidx.annotation.RequiresApi
+import java.lang.reflect.Array
 import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Method
 import java.util.Objects
 
+
 object StorageHelper {
 
-    // https://github.com/moneytoo/Player/blob/390c5a1aa9e433823af7b524d5812b13412c39ab/android-file-chooser/src/main/java/com/obsez/android/lib/filechooser/internals/FileUtil.java#L237
+    // https://github.com/moneytoo/Player/blob/master/android-file-chooser/src/main/java/com/obsez/android/lib/filechooser/internals/FileUtil.java
+
+    fun getStoragePaths(context: Context): LinkedHashMap<String?, String?> {
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            getStoragePathsLow(context)
+        } else {
+            getStoragePaths24(context)
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private fun getStoragePaths24(context: Context): LinkedHashMap<String?, String?> {
+        val paths = LinkedHashMap<String?, String?>()
+        val storageManager = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
+        try {
+            val result = Objects.requireNonNull(storageManager).storageVolumes
+            for (vol in result) {
+                Log.d("X", "  ---Object--" + vol + " | desc: " + vol.getDescription(context))
+                if (Build.VERSION.SDK_INT >= 30) {
+                    val dir = vol.directory ?: continue
+                    paths[dir.absolutePath] = formatPathAsLabel(dir.absolutePath)
+                } else {
+                    val getPath = vol.javaClass.getMethod("getPath")
+                    val path = getPath.invoke(vol) as String
+                    Log.d("X", "    ---path--$path")
+                    paths[path] = formatPathAsLabel(path)
+                }
+            }
+        } catch (e: InvocationTargetException) {
+            e.printStackTrace()
+        } catch (e: NoSuchMethodException) {
+            e.printStackTrace()
+        } catch (e: IllegalAccessException) {
+            e.printStackTrace()
+        } catch (e: java.lang.NullPointerException) {
+            e.printStackTrace()
+        }
+        if (paths.size == 0) {
+            val path = Environment.getExternalStorageDirectory().absolutePath
+            paths[path] = formatPathAsLabel(path)
+        }
+        return paths
+    }
+
+    private fun getStoragePathsLow(context: Context): LinkedHashMap<String?, String?> {
+        val paths = LinkedHashMap<String?, String?>()
+        val storageManager = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
+        try {
+            val storageVolumeClazz = Class.forName("android.os.storage.StorageVolume")
+            val getVolumeList = storageManager.javaClass.getMethod("getVolumeList")
+            val getPath = storageVolumeClazz.getMethod("getPath")
+            val result = getVolumeList.invoke(storageManager) as Any
+            val length: Int = Array.getLength(result)
+            Log.d("X", "---length--$length")
+            for (i in 0 until length) {
+                val storageVolumeElement: Any = Array.get(result, i) as Any
+                Log.d("X", "  ---Object--" + storageVolumeElement + "i==" + i)
+                val path = getPath.invoke(storageVolumeElement) as String
+                Log.d("X", "  ---path_total--$path")
+                Log.d("X", "    ---path--$path")
+                paths[path] = formatPathAsLabel(path)
+            }
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+        } catch (e: InvocationTargetException) {
+            e.printStackTrace()
+        } catch (e: NoSuchMethodException) {
+            e.printStackTrace()
+        } catch (e: IllegalAccessException) {
+            e.printStackTrace()
+        }
+        if (paths.size == 0) {
+            val path = Environment.getExternalStorageDirectory().absolutePath
+            paths[path] = formatPathAsLabel(path)
+        }
+        return paths
+    }
+
+    private fun formatPathAsLabel(path: String): String {
+        return "[ $path ]"
+    }
+
 
     fun getStoragePath(context: Context, isRemovable: Boolean): String {
         return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
@@ -66,11 +149,10 @@ object StorageHelper {
             val getPath: Method = storageVolumeClazz.getMethod("getPath")
             val isRemovableMtd: Method = storageVolumeClazz.getMethod("isRemovable")
             val result: Any = getVolumeList.invoke(storageManager) as Any
-            val length: Int = java.lang.reflect.Array.getLength(result)
-            //final int length = result.size();
+            val length: Int = Array.getLength(result)
             Log.d("X", "---length--$length")
             for (i in 0 until length) {
-                val storageVolumeElement: Any = java.lang.reflect.Array.get(result, i) as Any
+                val storageVolumeElement: Any = Array.get(result, i) as Any
                 Log.d("X", "  ---Object--" + storageVolumeElement + "i==" + i)
                 val path = getPath.invoke(storageVolumeElement) as String
                 Log.d("X", "  ---path_total--$path")
