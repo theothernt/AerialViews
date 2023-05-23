94761794da730b71c90c8da38c5e7e7e85d9b478
Neil Turner <neil@neilturner.com>
2023-05-23 15:44:15 +0100

    Removed unused code

--- app/src/main/kotlin/com/neilturner/aerialviews/utils/StorageHelper.kt
+++ app/src/main/kotlin/com/neilturner/aerialviews/utils/StorageHelper.kt
@@ -15,41 +15,6 @@ import java.util.Objects
 object StorageHelper {
     // https://github.com/moneytoo/Player/blob/master/android-file-chooser/src/main/java/com/obsez/android/lib/filechooser/internals/FileUtil.java
 
-    fun getStorageVols(context: Context): List<StorageVolume?> {
-        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-            getStorageVolsLow(context)
-        } else {
-            getStorageVols24(context)
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private fun getStorageVols24(context: Context): List<StorageVolume?> {
-        val storageManager = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
-        try {
-            return storageManager.storageVolumes
-        } catch (e: NullPointerException) {
-            e.printStackTrace()
-        }
-        return emptyList()
-    }
-
-    private fun getStorageVolsLow(context: Context): List<StorageVolume?> {
-        val storageManager = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
-        try {
-            val getVolumeList = storageManager.javaClass.getMethod("getVolumeList")
-            val result = getVolumeList.invoke(storageManager)
-            return result as List<StorageVolume?>
-        } catch (e: InvocationTargetException) {
-            e.printStackTrace()
-        } catch (e: NoSuchMethodException) {
-            e.printStackTrace()
-        } catch (e: IllegalAccessException) {
-            e.printStackTrace()
-        }
-        return emptyList()
-    }
-
     fun getStoragePaths(context: Context): LinkedHashMap<String?, String?> {
         return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
             getStoragePathsLow(context)
@@ -128,82 +93,5 @@ object StorageHelper {
 
     private fun formatPathAsLabel(path: String): String {
         return "[ $path ]"
-    }
-
-    fun getStoragePath(context: Context, isRemovable: Boolean): String {
-        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-            getStoragePathLow(context, isRemovable)
-        } else {
-            getStoragePath24(context, isRemovable)
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private fun getStoragePath24(context: Context, isRemovable: Boolean): String {
-        val storageManager = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
-        try {
-            val result: List<StorageVolume> =
-                Objects.requireNonNull(storageManager).storageVolumes
-            for (vol in result) {
-                Log.d("X", "  ---Object--" + vol + " | desc: " + vol.getDescription(context))
-                if (isRemovable != vol.isRemovable) {
-                    continue
-                }
-                return if (Build.VERSION.SDK_INT >= 30) {
-                    // TODO: Handle multiple removable volumes
-                    val dir = vol.directory ?: continue
-                    dir.absolutePath
-                } else {
-                    val getPath: Method = vol.javaClass.getMethod("getPath")
-                    val path = getPath.invoke(vol) as String
-                    Log.d("X", "    ---path--$path")
-                    // HACK
-                    if (isRemovable && result.size > 2 && path.startsWith("/storage/")) "/storage" else path
-                }
-            }
-        } catch (e: InvocationTargetException) {
-            e.printStackTrace()
-        } catch (e: NoSuchMethodException) {
-            e.printStackTrace()
-        } catch (e: IllegalAccessException) {
-            e.printStackTrace()
-        } catch (e: NullPointerException) {
-            e.printStackTrace()
-        }
-        return Environment.getExternalStorageDirectory().absolutePath
-    }
-
-    private fun getStoragePathLow(context: Context, isRemovable: Boolean): String {
-        val storageManager = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
-        try {
-            val storageVolumeClazz = Class.forName("android.os.storage.StorageVolume")
-            val getVolumeList: Method = storageManager.javaClass.getMethod("getVolumeList")
-            val getPath: Method = storageVolumeClazz.getMethod("getPath")
-            val isRemovableMtd: Method = storageVolumeClazz.getMethod("isRemovable")
-            val result: Any = getVolumeList.invoke(storageManager) as Any
-            val length: Int = Array.getLength(result)
-            Log.d("X", "---length--$length")
-            for (i in 0 until length) {
-                val storageVolumeElement: Any = Array.get(result, i) as Any
-                Log.d("X", "  ---Object--" + storageVolumeElement + "i==" + i)
-                val path = getPath.invoke(storageVolumeElement) as String
-                Log.d("X", "  ---path_total--$path")
-                val removable = isRemovableMtd.invoke(storageVolumeElement) as Boolean
-                if (isRemovable == removable) {
-                    Log.d("X", "    ---path--$path")
-                    // HACK
-                    return if (isRemovable && length > 2 && path.startsWith("/storage/")) "/storage" else path
-                }
-            }
-        } catch (e: ClassNotFoundException) {
-            e.printStackTrace()
-        } catch (e: InvocationTargetException) {
-            e.printStackTrace()
-        } catch (e: NoSuchMethodException) {
-            e.printStackTrace()
-        } catch (e: IllegalAccessException) {
-            e.printStackTrace()
-        }
-        return Environment.getExternalStorageDirectory().absolutePath
     }
 }
