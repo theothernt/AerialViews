38248dfa117b9cdea298e3064145d2927f4468f9
Neil Turner <neil@neilturner.com>
2023-10-10 16:14:45 +0100

    More refactoring

--- app/src/main/java/com/neilturner/aerialviews/ui/screensaver/VideoController.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/screensaver/VideoController.kt
@@ -3,31 +3,23 @@ package com.neilturner.aerialviews.ui.screensaver
 import android.content.Context
 import android.graphics.Typeface
 import android.util.Log
-import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.helper.widget.Flow
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat.generateViewId
-import androidx.core.widget.TextViewCompat
 import androidx.databinding.DataBindingUtil
 import com.neilturner.aerialviews.R
 import com.neilturner.aerialviews.databinding.AerialActivityBinding
 import com.neilturner.aerialviews.databinding.VideoViewBinding
 import com.neilturner.aerialviews.models.VideoPlaylist
-import com.neilturner.aerialviews.models.enums.OverlayType
-import com.neilturner.aerialviews.models.enums.SlotType
 import com.neilturner.aerialviews.models.prefs.GeneralPrefs
 import com.neilturner.aerialviews.models.prefs.InterfacePrefs
 import com.neilturner.aerialviews.models.videos.AerialVideo
 import com.neilturner.aerialviews.services.VideoService
-import com.neilturner.aerialviews.ui.overlays.AltTextClock
-import com.neilturner.aerialviews.ui.overlays.TextLocation
 import com.neilturner.aerialviews.ui.screensaver.ExoPlayerView.OnPlayerEventListener
 import com.neilturner.aerialviews.utils.FontHelper
-import com.neilturner.aerialviews.utils.SlotHelper
+import com.neilturner.aerialviews.utils.OverlayHelper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -42,7 +34,6 @@ class VideoController(private val context: Context) : OnPlayerEventListener {
     private var previousVideo = false
     private var canSkip = false
 
-    private val layout: ConstraintLayout
     private val videoView: VideoViewBinding
     private val loadingView: View
     private var loadingText: TextView
@@ -65,7 +56,6 @@ class VideoController(private val context: Context) : OnPlayerEventListener {
         loadingText = binding.loadingView.loadingText
 
         videoView = binding.videoView
-        layout = videoView.layout
         player = videoView.player
         player.setOnPlayerListener(this)
 
@@ -79,30 +69,10 @@ class VideoController(private val context: Context) : OnPlayerEventListener {
         typeface = FontHelper.getTypeface(context)
         loadingText.typeface = typeface
 
-        // OverlayHelper.setupOverlays(context, typeface, InterfacePrefs, GeneralPrefs)
-
-        val views = mutableListOf<View?>()
-        for (type in SlotType.entries) {
-            val view = getOverlayForSlot(type)
-            view?.id = generateViewId()
-            views.add(view)
-            if (view != null) {
-                layout.addView(view)
-            }
+        OverlayHelper.buildOverlayIds(context, videoView, typeface, InterfacePrefs).run {
+            bottomLeftIds = first
+            bottomRightIds = second
         }
-
-        // Build id lists for the 4 corners
-        // If both slots are empty, add empty view
-
-        // TEMP
-        val view = TextView(context)
-        view.height = 0
-        view.width = 0
-        layout.addView(view)
-
-        val ids = buildReferenceIds(views[0], views[1], views[2], views[3], view)
-        bottomLeftIds = ids.first
-        bottomRightIds = ids.second
 
         coroutineScope.launch {
             playlist = VideoService(context).fetchVideos()
@@ -148,66 +118,6 @@ class VideoController(private val context: Context) : OnPlayerEventListener {
 
         player.setUri(video.uri)
         player.start()
-    }
-
-    private fun getOverlayForSlot(slotType: SlotType): View? {
-        val prefs = SlotHelper.slotPrefs()
-        val overlay = prefs.find { it.second == slotType }
-        return getOverlay(overlay?.first)
-    }
-
-    private fun getOverlay(type: OverlayType?): View? {
-        when (type) {
-            OverlayType.CLOCK -> {
-                val clock = AltTextClock(context)
-                TextViewCompat.setTextAppearance(clock, R.style.ClockText)
-                clock.typeface = typeface
-                clock.setTextSize(TypedValue.COMPLEX_UNIT_SP, InterfacePrefs.clockSize.toFloat())
-                return clock
-            }
-            OverlayType.LOCATION -> {
-                val location = TextLocation(context)
-                TextViewCompat.setTextAppearance(location, R.style.LocationText)
-                location.setTextSize(TypedValue.COMPLEX_UNIT_SP, InterfacePrefs.locationSize.toFloat())
-                location.typeface = typeface
-                location.text = "Somewhere lovely!"
-                return location
-            }
-            OverlayType.DATE -> {
-                val date = TextView(context)
-                TextViewCompat.setTextAppearance(date, R.style.LocationText)
-                date.setTextSize(TypedValue.COMPLEX_UNIT_SP, InterfacePrefs.locationSize.toFloat())
-                date.typeface = typeface
-                date.text = "Today's date!"
-                return date
-            }
-
-            OverlayType.MUSIC -> {
-                val location = TextLocation(context)
-                TextViewCompat.setTextAppearance(location, R.style.LocationText)
-                location.setTextSize(TypedValue.COMPLEX_UNIT_SP, InterfacePrefs.locationSize.toFloat())
-                location.typeface = typeface
-                location.text = "Music 🎶"
-                return location
-            }
-            else -> {
-                return null
-            }
-        }
-    }
-
-    private fun buildReferenceIds(left1: View?, left2: View?, right1: View?, right2: View?, empty: View): Pair<List<Int>, List<Int>> {
-        var leftIds = listOfNotNull(left1?.id, left2?.id)
-        var rightIds = listOfNotNull(right1?.id, right2?.id)
-
-        if (leftIds.isEmpty() && rightIds.isNotEmpty()) {
-            leftIds = listOf(empty.id)
-        }
-
-        if (leftIds.isNotEmpty() && rightIds.isEmpty()) {
-            rightIds = listOf(empty.id)
-        }
-        return Pair(leftIds, rightIds)
     }
 
     private fun fadeOutLoading() {
--- app/src/main/java/com/neilturner/aerialviews/utils/OverlayHelper.kt
+++ app/src/main/java/com/neilturner/aerialviews/utils/OverlayHelper.kt
@@ -2,10 +2,91 @@ package com.neilturner.aerialviews.utils
 
 import android.content.Context
 import android.graphics.Typeface
+import android.util.TypedValue
+import android.view.View
+import android.widget.TextView
+import androidx.core.view.ViewCompat
+import androidx.core.widget.TextViewCompat
+import com.neilturner.aerialviews.R
+import com.neilturner.aerialviews.databinding.VideoViewBinding
+import com.neilturner.aerialviews.models.enums.OverlayType
+import com.neilturner.aerialviews.models.enums.SlotType
 import com.neilturner.aerialviews.models.prefs.InterfacePrefs
+import com.neilturner.aerialviews.ui.overlays.AltTextClock
+import com.neilturner.aerialviews.ui.overlays.TextLocation
 
 object OverlayHelper {
 
-    fun setupOverlays(context: Context, typeface: Typeface, prefs: InterfacePrefs) {
+    fun buildOverlayIds(context: Context, root: VideoViewBinding, typeface: Typeface?, prefs: InterfacePrefs): Pair<List<Int>, List<Int>> {
+        val views = mutableListOf<View?>()
+        val slots = SlotHelper.slotPrefs()
+
+        for (type in SlotType.entries) {
+            val overlay = slots.find { it.second == type }
+            val view = getOverlay(context, typeface, prefs, overlay?.first)
+            view?.id = ViewCompat.generateViewId()
+            views.add(view)
+            if (view != null) {
+                root.layout.addView(view)
+            }
+        }
+
+        val bottomEmptyView = root.emptyViewBottom
+        // val topEmptyView = TextView(context)
+        return buildReferenceIds(views[0], views[1], views[2], views[3], bottomEmptyView)
+    }
+
+    private fun buildReferenceIds(left1: View?, left2: View?, right1: View?, right2: View?, empty: View): Pair<List<Int>, List<Int>> {
+        var leftIds = listOfNotNull(left1?.id, left2?.id)
+        var rightIds = listOfNotNull(right1?.id, right2?.id)
+
+        if (leftIds.isEmpty() && rightIds.isNotEmpty()) {
+            leftIds = listOf(empty.id)
+        }
+
+        if (leftIds.isNotEmpty() && rightIds.isEmpty()) {
+            rightIds = listOf(empty.id)
+        }
+        return Pair(leftIds, rightIds)
+    }
+
+    private fun getOverlay(context: Context, typeface: Typeface?, prefs: InterfacePrefs, type: OverlayType?): View? {
+        when (type) {
+            OverlayType.CLOCK -> {
+                val clock = AltTextClock(context)
+                TextViewCompat.setTextAppearance(clock, R.style.ClockText)
+                clock.typeface = typeface
+                clock.setTextSize(TypedValue.COMPLEX_UNIT_SP, InterfacePrefs.clockSize.toFloat())
+                return clock
+            }
+            OverlayType.LOCATION -> {
+                val location = TextLocation(context)
+                TextViewCompat.setTextAppearance(location, R.style.LocationText)
+                location.setTextSize(TypedValue.COMPLEX_UNIT_SP, InterfacePrefs.locationSize.toFloat())
+                location.typeface = typeface
+                location.text = "Somewhere lovely!"
+                return location
+            }
+            OverlayType.DATE -> {
+                val date = TextView(context)
+                TextViewCompat.setTextAppearance(date, R.style.LocationText)
+                date.setTextSize(TypedValue.COMPLEX_UNIT_SP, InterfacePrefs.locationSize.toFloat())
+                date.typeface = typeface
+                date.text = "Today's date!"
+                return date
+            }
+
+            OverlayType.MUSIC -> {
+                val location = TextLocation(context)
+                TextViewCompat.setTextAppearance(location, R.style.LocationText)
+                location.setTextSize(TypedValue.COMPLEX_UNIT_SP, InterfacePrefs.locationSize.toFloat())
+                location.typeface = typeface
+                location.text = "Music 🎶"
+                return location
+            }
+            else -> {
+                return null
+            }
+        }
     }
 }
