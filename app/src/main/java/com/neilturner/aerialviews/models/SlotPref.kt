1f47c9f8f738b9d20ae4317c0264bafc26de204b
Neil Turner <neil@neilturner.com>
2023-10-21 17:28:08 +0100

    Lint fix

--- app/src/main/java/com/neilturner/aerialviews/models/SlotPref.kt
+++ app/src/main/java/com/neilturner/aerialviews/models/SlotPref.kt
@@ -3,4 +3,4 @@ package com.neilturner.aerialviews.models
 import com.neilturner.aerialviews.models.enums.OverlayType
 import com.neilturner.aerialviews.models.enums.SlotType
 
-data class SlotPref (val pref: OverlayType, val type: SlotType, val label: String)
+data class SlotPref(val pref: OverlayType, val type: SlotType, val label: String)
