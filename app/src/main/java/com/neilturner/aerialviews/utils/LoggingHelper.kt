6b07854aedc62cd38d1990eaaa31895d8a284510
Neil Turner <neil@neilturner.com>
2023-10-25 15:32:00 +0100

    Update namespace and syntax

--- app/src/main/java/com/neilturner/aerialviews/utils/LoggingHelper.kt
+++ app/src/main/java/com/neilturner/aerialviews/utils/LoggingHelper.kt
@@ -1,16 +1,20 @@
 package com.neilturner.aerialviews.utils
 
+import androidx.core.os.bundleOf
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.analytics.ktx.logEvent
 import com.google.firebase.ktx.Firebase
 
 object LoggingHelper {
 
+    private val firebaseAnalytics  = Firebase.analytics
     fun logScreenView(screenName: String, activityName: String) {
-        Firebase.analytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
-            param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
-            param(FirebaseAnalytics.Param.SCREEN_CLASS, activityName)
-        }
+
+        val parameters = bundleOf (
+            Pair(FirebaseAnalytics.Param.SCREEN_NAME, screenName),
+            Pair(FirebaseAnalytics.Param.SCREEN_CLASS, activityName)
+        )
+
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, parameters)
     }
 }
