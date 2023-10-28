cf48aaf32dfa9a3cf79e37ba517894910d3d71b1
Neil Turner <neil@neilturner.com>
2023-10-28 22:00:13 +0100

    Add message control

---
+++ app/src/main/java/com/neilturner/aerialviews/ui/overlays/TextMessage.kt
@@ -0,0 +1,30 @@
+@file:Suppress("unused")
+
+package com.neilturner.aerialviews.ui.overlays
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.widget.TextViewCompat
+import com.neilturner.aerialviews.R
+import com.neilturner.aerialviews.models.enums.DateType
+import com.neilturner.aerialviews.utils.DateHelper
+
+class TextMessage : AppCompatTextView {
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        TextViewCompat.setTextAppearance(this, R.style.OverlayText)
+    }
+
+    fun updateMessage(message: String) {
+        this.text = message
+    }
+
+    companion object {
+        private const val TAG = "TextMessage"
+    }
+}
