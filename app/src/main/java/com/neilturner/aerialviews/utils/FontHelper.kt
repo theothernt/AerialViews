34218082246efaf0dbfcbc712e32374bd2ec942d
Neil Turner <neil@neilturner.com>
2023-11-03 16:54:40 +0000

    Try/catch for font loading

--- app/src/main/java/com/neilturner/aerialviews/utils/FontHelper.kt
+++ app/src/main/java/com/neilturner/aerialviews/utils/FontHelper.kt
@@ -2,6 +2,7 @@ package com.neilturner.aerialviews.utils
 
 import android.content.Context
 import android.graphics.Typeface
+import android.util.Log
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.TypefaceCompat
 import com.neilturner.aerialviews.R
@@ -10,11 +11,18 @@ import com.neilturner.aerialviews.models.prefs.GeneralPrefs
 object FontHelper {
 
     fun getTypeface(context: Context): Typeface {
-        val font = if (GeneralPrefs.fontTypeface == "open-sans") {
-            ResourcesCompat.getFont(context, R.font.opensans)
-        } else {
+        val font = try {
+            if (GeneralPrefs.fontTypeface == "open-sans") {
+                ResourcesCompat.getFont(context, R.font.opensans)
+            } else {
+                Typeface.create("san-serif", Typeface.NORMAL)
+            }
+        } catch (ex: Exception) {
+            Log.e(TAG, ex.message.toString())
             Typeface.create("san-serif", Typeface.NORMAL)
         }
         return TypefaceCompat.create(context, font, GeneralPrefs.fontWeight.toInt(), false)
     }
+
+    private const val TAG = "FontHelper"
 }
