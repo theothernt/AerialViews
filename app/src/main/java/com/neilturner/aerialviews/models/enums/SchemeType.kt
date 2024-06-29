7933876286a3fee400c6aa555babfc4426d589ae
Neil Turner <neil@neilturner.com>
2024-06-29 16:00:32 +0100

    Added UI prefs

---
+++ app/src/main/java/com/neilturner/aerialviews/models/enums/SchemeType.kt
@@ -0,0 +1,6 @@
+package com.neilturner.aerialviews.models.enums
+
+enum class SchemeType {
+    HTTP,
+    HTTPS
+}
--- app/src/main/java/com/neilturner/aerialviews/models/prefs/WebDavMediaPrefs.kt
+++ app/src/main/java/com/neilturner/aerialviews/models/prefs/WebDavMediaPrefs.kt
@@ -3,17 +3,17 @@ package com.neilturner.aerialviews.models.prefs
 import com.chibatching.kotpref.KotprefModel
 import com.chibatching.kotpref.enumpref.nullableEnumValuePref
 import com.neilturner.aerialviews.models.enums.ProviderMediaType
+import com.neilturner.aerialviews.models.enums.SchemeType
 
 object WebDavMediaPrefs : KotprefModel() {
     override val kotprefName = "${context.packageName}_preferences"
 
     var enabled by booleanPref(false, "webdav_media_enabled")
     var mediaType by nullableEnumValuePref(ProviderMediaType.VIDEOS, "webdav_media_type")
-    var scheme by stringPref("", "webdav_media_scheme")
+    var scheme by nullableEnumValuePref(SchemeType.HTTP, "webdav_media_scheme")
     var hostName by stringPref("", "webdav_media_hostname")
     var pathName by stringPref("", "webdav_media_pathname")
-    var port by stringPref("", "webdav_media_port")
     var userName by stringPref("", "webdav_media_username")
     var password by stringPref("", "webdav_media_password")
-    var searchSubfolders by booleanPref(false, "webdav_media_search_subfolders")
+    //var searchSubfolders by booleanPref(false, "webdav_media_search_subfolders")
 }
