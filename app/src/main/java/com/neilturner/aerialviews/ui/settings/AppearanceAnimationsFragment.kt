b634b5825046b200bb8755a69a18518127f64c9f
Neil Turner <neil@neilturner.com>
2024-08-18 17:46:07 +0100

    Re-arrange some prefs and strings

--- app/src/main/java/com/neilturner/aerialviews/ui/settings/AppearanceAnimationsFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/AppearanceAnimationsFragment.kt
@@ -5,20 +5,20 @@ import androidx.preference.PreferenceFragmentCompat
 import com.neilturner.aerialviews.R
 import com.neilturner.aerialviews.utils.LoggingHelper
 
-class AppearanceTransitionsFragment : PreferenceFragmentCompat() {
+class AppearanceAnimationsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(
         savedInstanceState: Bundle?,
         rootKey: String?,
     ) {
-        setPreferencesFromResource(R.xml.settings_appearance_transitions, rootKey)
+        setPreferencesFromResource(R.xml.settings_appearance_animations, rootKey)
     }
 
     override fun onResume() {
         super.onResume()
-        LoggingHelper.logScreenView("Transitions", TAG)
+        LoggingHelper.logScreenView("Animations", TAG)
     }
 
     companion object {
-        private const val TAG = "TransitionsFragment"
+        private const val TAG = "AnimationsFragment"
     }
 }
