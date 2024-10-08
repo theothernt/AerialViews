2e8e5383b5a38c05b838a2042eb0a4051dbfd1f5
Neil Turner <neilturner@gmail.com>
2024-10-08 21:08:32 +0100

    A better test

--- app/src/main/java/com/neilturner/aerialviews/ui/overlays/ProgressBar.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/overlays/ProgressBar.kt
@@ -1,40 +1,54 @@
 package com.neilturner.aerialviews.ui.overlays
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 class ProgressBar @JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null) :
     View(context, attrs) {
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
     private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var progress: Float = 0F
+    private var newWidth = 0
 
     init {
         paint.style = Paint.Style.FILL
         paint.setColor(COLOR)
 
         coroutineScope.launch {
-            1.until(100).forEach { index ->
-                delay(1000)
-                progress = index / 100f
-                //postInvalidate()
-            }
+//            1.until(100).forEach { index ->
+//                delay(1000)
+//                progress = index / 100f
+//                //postInvalidate()
+//            }
+
+            delay(3_000)
+            animateWidth()
         }
     }
 
-    @Synchronized
+    fun animateWidth() {
+        val parentWidth = (this.parent as View).measuredWidth
+        val widthAnimator = ValueAnimator.ofInt(0, parentWidth)
+        widthAnimator.duration = 10_000
+        widthAnimator.interpolator = LinearInterpolator()
+        widthAnimator.addUpdateListener { animation ->
+            layoutParams.width = (animation.animatedValue as Int)
+            requestLayout()
+        }
+        widthAnimator.start()
+    }
+
     override fun onDraw(canvas: Canvas) {
-        Timber.i("onDraw - $progress")
-        val x = progress * width
-        canvas.drawRect(0f, 0f, x, height.toFloat(), paint)
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
         postInvalidate()
     }
 
