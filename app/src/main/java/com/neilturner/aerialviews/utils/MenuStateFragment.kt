470b778ed5126504ec19fb12edcef2ef9224419f
Neil Turner <neil@neilturner.com>
2024-10-26 13:03:20 +0100

    Some refactoring

--- app/src/main/java/com/neilturner/aerialviews/utils/MenuStateFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/utils/MenuStateFragment.kt
@@ -4,13 +4,18 @@ import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceFragmentCompat
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 abstract class MenuStateFragment : PreferenceFragmentCompat() {
-
     private var position = -1
 
     override fun onResume() {
         super.onResume()
+
+        if (DeviceHelper.isTV(requireContext())) {
+            return
+        }
+
         lifecycleScope.launch {
             delay(50)
             if (position != -1) {
@@ -22,12 +27,17 @@ abstract class MenuStateFragment : PreferenceFragmentCompat() {
 
     override fun onPause() {
         super.onPause()
+
+        if (DeviceHelper.isTV(requireContext())) {
+            return
+        }
+
         val view = listView.findFocus()
-        if (view != null && DeviceHelper.isTV(requireContext())) {
+        view?.let {
             try {
                 position = listView.layoutManager?.getPosition(view) ?: -1
             } catch (e: Exception) {
-                // Ignore
+                Timber.e(e)
             }
         }
     }
