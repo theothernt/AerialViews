877cf71f3abc3d800dec9a168cf6d4d100fbcf9d
Neil Turner <neil@neilturner.com>
2024-11-03 17:58:49 +0000

    Move scaling options to Appearance section

---
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/AppearanceScalingFragment.kt
@@ -0,0 +1,20 @@
+package com.neilturner.aerialviews.ui.settings
+
+import android.os.Bundle
+import com.neilturner.aerialviews.R
+import com.neilturner.aerialviews.utils.FirebaseHelper
+import com.neilturner.aerialviews.utils.MenuStateFragment
+
+class AppearanceScalingFragment : MenuStateFragment() {
+    override fun onCreatePreferences(
+        savedInstanceState: Bundle?,
+        rootKey: String?,
+    ) {
+        setPreferencesFromResource(R.xml.settings_appearance_scaling, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        FirebaseHelper.logScreenView("Scaling", this)
+    }
+}
