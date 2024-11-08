2406a975381e13e1ed70a54d0aecc1c3d8673b2e
Neil Turner <neilturner@gmail.com>
2024-11-08 19:09:09 +0000

    Change to make things build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     //kotlin("jvm") version libs.versions.kotlin.get()
-    kotlin("kapt") version libs.versions.kotlin.get()
+    //kotlin("kapt") version libs.versions.kotlin.get()
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.ksp) apply false
