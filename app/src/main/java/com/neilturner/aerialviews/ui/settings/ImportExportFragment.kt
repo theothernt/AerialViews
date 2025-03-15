282a7346ca7799598a987e0ba5c25e3191cacfc8
Neil Turner <neil@neilturner.com>
2025-03-15 15:49:34 +0000

    Added basic screen layout and text

--- app/src/main/java/com/neilturner/aerialviews/ui/settings/ImportExportFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/ImportExportFragment.kt
@@ -3,11 +3,17 @@
 package com.neilturner.aerialviews.ui.settings
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.preference.Preference
+import androidx.preference.PreferenceManager
 import com.neilturner.aerialviews.R
 import com.neilturner.aerialviews.utils.FirebaseHelper
 import com.neilturner.aerialviews.utils.MenuStateFragment
+import kotlinx.coroutines.launch
 
-class ImportExportFragment : MenuStateFragment() {
+class ImportExportFragment : MenuStateFragment(),
+    PreferenceManager.OnPreferenceTreeClickListener {
     override fun onCreatePreferences(
         savedInstanceState: Bundle?,
         rootKey: String?,
@@ -18,5 +24,20 @@ class ImportExportFragment : MenuStateFragment() {
     override fun onResume() {
         super.onResume()
         FirebaseHelper.logScreenView("ImportExport", this)
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+
+        if (preference.key.contains("export_settings")) {
+            lifecycleScope.launch { exportSettings() }
+            return true
+        }
+
+        return super.onPreferenceTreeClick(preference)
+    }
+
+    private fun exportSettings() {
+        // Export settings
+        Toast.makeText(requireContext(), "Settings exported", Toast.LENGTH_SHORT).show()
     }
 }
