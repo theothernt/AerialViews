894c0ce11c0a9a94e6c3928cf5f47e51bc5581ad
Neil Turner <neilturner@gmail.com>
2025-03-19 19:38:17 +0000

    Added write permission request

--- app/src/main/java/com/neilturner/aerialviews/ui/settings/ImportExportFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/ImportExportFragment.kt
@@ -6,23 +6,48 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.Preference
 import androidx.preference.PreferenceManager
 import com.neilturner.aerialviews.R
+import com.neilturner.aerialviews.utils.DialogHelper
 import com.neilturner.aerialviews.utils.FirebaseHelper
 import com.neilturner.aerialviews.utils.MenuStateFragment
+import com.neilturner.aerialviews.utils.PermissionHelper
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class ImportExportFragment :
     MenuStateFragment(),
     PreferenceManager.OnPreferenceTreeClickListener {
+    private lateinit var requestWritePermission: ActivityResultLauncher<String>
+
     override fun onCreatePreferences(
         savedInstanceState: Bundle?,
         rootKey: String?,
     ) {
         setPreferencesFromResource(R.xml.settings_import_export, rootKey)
+
+        requestWritePermission =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission(),
+            ) { isGranted: Boolean ->
+                if (!isGranted) {
+                    lifecycleScope.launch {
+                        DialogHelper.show(
+                            requireContext(),
+                            resources.getString(R.string.samba_videos_export_failed),
+                            resources.getString(R.string.samba_videos_permission_denied),
+                        )
+                    }
+                } else {
+                    lifecycleScope.launch {
+                        exportSettings()
+                    }
+                }
+            }
 
         lifecycleScope.launch {
             processDataUri()
@@ -35,10 +60,11 @@ class ImportExportFragment :
     }
 
     private fun processDataUri() {
-        val dataUri: Uri? =
+         val dataUri: Uri? =
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 arguments?.getParcelable("dataUri", Uri::class.java)
             } else {
+                @Suppress("DEPRECATION")
                 arguments?.getParcelable("dataUri")
             }
 
@@ -47,15 +73,24 @@ class ImportExportFragment :
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
         if (preference.key.contains("export_settings")) {
-            lifecycleScope.launch { exportSettings() }
+            checkWritePermission()
             return true
         }
-
         return super.onPreferenceTreeClick(preference)
     }
 
+    private fun checkWritePermission() {
+        lifecycleScope.launch {
+            val hasPermission = PermissionHelper.hasDocumentWritePermission(requireContext())
+            if (!hasPermission) {
+                requestWritePermission.launch(PermissionHelper.getWriteDocumentPermission())
+            } else {
+                exportSettings()
+            }
+        }
+    }
+
     private fun exportSettings() {
-        // Export settings
         Toast.makeText(requireContext(), "Settings exported", Toast.LENGTH_SHORT).show()
     }
 }
