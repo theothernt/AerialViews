910b98e683e7a2f93c8cbee0807a5ff87b90b47d
Neil Turner <neilturner@gmail.com>
2025-05-14 21:01:18 +0100

    Add custom control for two pref layout

--- app/src/main/java/com/neilturner/aerialviews/ui/settings/OverlaysRowBottomFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/OverlaysRowBottomFragment.kt
@@ -6,6 +6,7 @@ import androidx.preference.ListPreference
 import com.neilturner.aerialviews.R
 import com.neilturner.aerialviews.models.enums.OverlayType
 import com.neilturner.aerialviews.models.prefs.GeneralPrefs
+import com.neilturner.aerialviews.ui.preference.DualDropdownPreference
 import com.neilturner.aerialviews.utils.FirebaseHelper
 import com.neilturner.aerialviews.utils.MenuStateFragment
 import com.neilturner.aerialviews.utils.SlotHelper
@@ -18,6 +19,30 @@ class OverlaysRowBottomFragment :
         rootKey: String?,
     ) {
         setPreferencesFromResource(R.xml.settings_overlays_row_bottom, rootKey)
+        setupDualDropdowns()
+    }
+
+    private fun setupDualDropdowns() {
+        // Get the original ListPreferences (they are set to invisible in XML)
+        val bottomLeft1 = preferenceScreen.findPreference<ListPreference>("slot_bottom_left1")
+        val bottomLeft2 = preferenceScreen.findPreference<ListPreference>("slot_bottom_left2")
+        val bottomRight1 = preferenceScreen.findPreference<ListPreference>("slot_bottom_right1")
+        val bottomRight2 = preferenceScreen.findPreference<ListPreference>("slot_bottom_right2")
+
+        // Get the dual preference containers
+        val dualBottomLeft = preferenceScreen.findPreference<DualDropdownPreference>("dual_bottom_left")
+        val dualBottomRight = preferenceScreen.findPreference<DualDropdownPreference>("dual_bottom_right")
+
+        // Set up the dual preferences
+        dualBottomLeft?.apply {
+            bottomLeft2?.let { setLeftPreference(it) }
+            bottomLeft1?.let { setRightPreference(it) }
+        }
+
+        dualBottomRight?.apply {
+            bottomRight2?.let { setLeftPreference(it) }
+            bottomRight1?.let { setRightPreference(it) }
+        }
     }
 
     override fun onResume() {
@@ -61,5 +86,8 @@ class OverlaysRowBottomFragment :
         SlotHelper.buildOverlayList(bottomLeft2, overlayData.first, overlayData.second, slotPrefs)
         SlotHelper.buildOverlayList(bottomRight1, overlayData.first, overlayData.second, slotPrefs)
         SlotHelper.buildOverlayList(bottomRight2, overlayData.first, overlayData.second, slotPrefs)
+
+        // Update dual preferences after list preferences have been updated
+        setupDualDropdowns()
     }
 }
--- app/src/main/java/com/neilturner/aerialviews/ui/settings/OverlaysRowTopFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/OverlaysRowTopFragment.kt
@@ -6,6 +6,7 @@ import androidx.preference.ListPreference
 import com.neilturner.aerialviews.R
 import com.neilturner.aerialviews.models.enums.OverlayType
 import com.neilturner.aerialviews.models.prefs.GeneralPrefs
+import com.neilturner.aerialviews.ui.preference.DualDropdownPreference
 import com.neilturner.aerialviews.utils.FirebaseHelper
 import com.neilturner.aerialviews.utils.MenuStateFragment
 import com.neilturner.aerialviews.utils.SlotHelper
@@ -18,6 +19,30 @@ class OverlaysRowTopFragment :
         rootKey: String?,
     ) {
         setPreferencesFromResource(R.xml.settings_overlays_row_top, rootKey)
+        setupDualDropdowns()
+    }
+
+    private fun setupDualDropdowns() {
+        // Get the original ListPreferences (they are set to invisible in XML)
+        val topLeft1 = preferenceScreen.findPreference<ListPreference>("slot_top_left1")
+        val topLeft2 = preferenceScreen.findPreference<ListPreference>("slot_top_left2")
+        val topRight1 = preferenceScreen.findPreference<ListPreference>("slot_top_right1")
+        val topRight2 = preferenceScreen.findPreference<ListPreference>("slot_top_right2")
+
+        // Get the dual preference containers
+        val dualTopLeft = preferenceScreen.findPreference<DualDropdownPreference>("dual_top_left")
+        val dualTopRight = preferenceScreen.findPreference<DualDropdownPreference>("dual_top_right")
+
+        // Set up the dual preferences
+        dualTopLeft?.apply {
+            topLeft2?.let { setLeftPreference(it) }
+            topLeft1?.let { setRightPreference(it) }
+        }
+
+        dualTopRight?.apply {
+            topRight2?.let { setLeftPreference(it) }
+            topRight1?.let { setRightPreference(it) }
+        }
     }
 
     override fun onResume() {
@@ -61,5 +86,8 @@ class OverlaysRowTopFragment :
         SlotHelper.buildOverlayList(topLeft2, overlayData.first, overlayData.second, slotPrefs)
         SlotHelper.buildOverlayList(topRight1, overlayData.first, overlayData.second, slotPrefs)
         SlotHelper.buildOverlayList(topRight2, overlayData.first, overlayData.second, slotPrefs)
+
+        // Update dual preferences after list preferences have been updated
+        setupDualDropdowns()
     }
 }
