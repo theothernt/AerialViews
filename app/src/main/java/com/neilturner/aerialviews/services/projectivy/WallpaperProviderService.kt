7fe2c221dc1645f06328a6426df7b6878ff570a3
Neil Turner <neil@neilturner.com>
2025-06-06 17:42:48 +0100

    Return existing Jetson videos

--- app/src/main/java/com/neilturner/aerialviews/services/projectivy/WallpaperProviderService.kt
+++ app/src/main/java/com/neilturner/aerialviews/services/projectivy/WallpaperProviderService.kt
@@ -3,9 +3,14 @@ package com.neilturner.aerialviews.services.projectivy
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import com.neilturner.aerialviews.models.prefs.Comm1VideoPrefs
+import com.neilturner.aerialviews.providers.Comm1MediaProvider
+import kotlinx.coroutines.runBlocking
 import tv.projectivy.plugin.wallpaperprovider.api.Event
 import tv.projectivy.plugin.wallpaperprovider.api.IWallpaperProviderService
 import tv.projectivy.plugin.wallpaperprovider.api.Wallpaper
+import tv.projectivy.plugin.wallpaperprovider.api.WallpaperDisplayMode
+import tv.projectivy.plugin.wallpaperprovider.api.WallpaperType
 
 class WallpaperProviderService : Service() {
     override fun onCreate() {
@@ -21,16 +26,23 @@ class WallpaperProviderService : Service() {
     private val binder =
         object : IWallpaperProviderService.Stub() {
             override fun getWallpapers(event: Event?): List<Wallpaper> {
-                val testUrl = "https://github.com/glouel/AerialCommunity/releases/download/mw2-1080p-sdr/video_inspire_florida_miami_brickell_sunset_00036.1080-sdr.mov"
                 return when (event) {
                     is Event.TimeElapsed -> {
-                        // This is where you generate the wallpaper list that will be cycled every x minute
-                        return listOf(
-                            // IMAGE can be served from app drawable/, local storage or internet
-                            // Wallpaper(PreferencesManager.imageUrl, WallpaperType.IMAGE, author = "Pixabay"),
-                            // VIDEO can be served from app raw/, local storage or internet (some formats might not be supported, though)
-                            Wallpaper(testUrl, WallpaperType.VIDEO, WallpaperDisplayMode.DEFAULT, title = "Miami Sunset"),
-                        )
+                        // Fetch all videos from Comm1MediaProvider
+                        val comm1Provider = Comm1MediaProvider(applicationContext, Comm1VideoPrefs)
+
+                        // Use runBlocking since the provider's fetchMedia is a suspend function
+                        val aerialMediaList = runBlocking { comm1Provider.fetchMedia() }
+
+                        // Convert AerialMedia objects to Wallpaper objects
+                        aerialMediaList.map { media ->
+                            Wallpaper(
+                                media.uri.toString(),
+                                WallpaperType.VIDEO,
+                                WallpaperDisplayMode.DEFAULT,
+                                title = media.description
+                            )
+                        }
                     }
                     else -> emptyList() // Returning an empty list won't change the currently displayed wallpaper
                 }
