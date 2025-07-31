aa66317a23fe46d059ec91f9683ec383b21273b2
Neil Turner <neilturner@gmail.com>
2025-07-31 18:23:46 +0100

    Missed a bit!

--- app/src/main/java/com/neilturner/aerialviews/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/SettingsFragment.kt
@@ -1,14 +1,11 @@
 package com.neilturner.aerialviews.ui.settings
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.preference.Preference
 import androidx.preference.PreferenceManager
 import com.neilturner.aerialviews.R
-import com.neilturner.aerialviews.models.prefs.ImmichMediaPrefs
 import com.neilturner.aerialviews.utils.FirebaseHelper
 import com.neilturner.aerialviews.utils.MenuStateFragment
-import timber.log.Timber
 
 class SettingsFragment :
     MenuStateFragment(),
@@ -18,19 +15,6 @@ class SettingsFragment :
         rootKey: String?,
     ) {
         setPreferencesFromResource(R.xml.settings, rootKey)
-
-        try {
-            val fav = ImmichMediaPrefs.includeFavorites
-            Timber.d("Include Favorites: $fav")
-        } catch (e: Exception) {
-            Toast
-                .makeText(
-                    requireContext(),
-                    "Error accessing Immich preferences: ${e.message}",
-                    Toast.LENGTH_LONG,
-                ).show()
-            e.printStackTrace()
-        }
     }
 
     override fun onResume() {
