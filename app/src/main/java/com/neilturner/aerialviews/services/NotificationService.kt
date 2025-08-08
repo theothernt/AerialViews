8fb8d5094c0825ee4daaf11bb82b7ca56babdf4b
Neil Turner <neil@neilturner.com>
2025-08-08 16:45:11 +0100

    Add more logging

--- app/src/main/java/com/neilturner/aerialviews/services/NotificationService.kt
+++ app/src/main/java/com/neilturner/aerialviews/services/NotificationService.kt
@@ -1,5 +1,27 @@
 package com.neilturner.aerialviews.services
 
 import android.service.notification.NotificationListenerService
+import timber.log.Timber
 
-class NotificationService : NotificationListenerService()
+class NotificationService : NotificationListenerService() {
+    
+    override fun onListenerConnected() {
+        super.onListenerConnected()
+        Timber.i("NotificationService connected")
+    }
+    
+    override fun onListenerDisconnected() {
+        super.onListenerDisconnected()
+        Timber.i("NotificationService disconnected")
+    }
+    
+    override fun onCreate() {
+        super.onCreate()
+        Timber.i("NotificationService created")
+    }
+    
+    override fun onDestroy() {
+        Timber.i("NotificationService destroyed")
+        super.onDestroy()
+    }
+}
