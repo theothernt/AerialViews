05834ed185351e80cf4afdef374007ab31700751
Neil Turner <neil@neilturner.com>
2025-08-18 18:07:41 +0100

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,9 +24,9 @@ android {
         applicationId = "com.neilturner.aerialviews"
         minSdk = 22 // to support Fire OS 5, Android v5.1, Lvl 22
         targetSdk = 36
-        versionCode = 65
+        versionCode = 66
         versionName = "1.7.9"
-        betaVersion = "-beta9"
+        betaVersion = "-beta10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
