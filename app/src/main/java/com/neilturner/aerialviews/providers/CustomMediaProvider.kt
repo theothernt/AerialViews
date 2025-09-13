53a3488b132e553a23e5f7fcd6528b6ef485bb29
Neil Turner <neilturner@gmail.com>
2025-09-13 18:31:16 +0100

    Add more context

--- app/src/main/java/com/neilturner/aerialviews/providers/CustomMediaProvider.kt
+++ app/src/main/java/com/neilturner/aerialviews/providers/CustomMediaProvider.kt
@@ -26,7 +26,7 @@ class CustomMediaProvider(
         // If URL is valid and contains JSON, quick validation with Regex for manifest/entries format
         // If any valid URLs left, run buildVideoAndMetadata to parse and build list of videos
 
-        // val urls = validateUrlsAndJsonContent
+        // val urls = validateUrlsAndJsonContent()
         // if (urls.isNotEmpty()) buildVideoAndMetadata()
 
         // Should return result - count + message or summary?
@@ -38,11 +38,37 @@ class CustomMediaProvider(
         return metadata
     }
 
-    private fun validateUrlsAndJsonContent() {
+    private fun validateUrlsAndJsonContent(): String {
 
+        return ""
     }
 
     private fun buildVideoAndMetadata() {
+
+        // Use retrofit
+        // an example of the entries JSON format is the Comm2Video model object
+        // an example of the manifest JSON format is:
+        /*
+
+            {
+            sources: [
+            {
+            name: "Community Videos from Joshua Michaels & Hal Bergman",
+            description: "Enjoy this contribution of aerial videos, if you want to support the artists and get more videos from them, press the More button below.",
+            scenes: [
+            "landscape"
+            ],
+            manifestUrl: "https://raw.githubusercontent.com/glouel/AerialCommunity/master/entries.json",
+            license: "https://jetsoncreative.com/aeriallicense",
+            more: "https://magicwindow.app/aerial",
+            local: false,
+            cacheable: true
+            }
+            ]
+            }
+
+        */
+        
         // For each URL, parse it as a manifest JSON format first (as opposed to an entries JSON format)
         // Return list of success (new entries JSON format URLs) and failures (might be a bad URL or entries JSON format URL)
 
