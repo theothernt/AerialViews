39006ccd6ddcc953a44014b7f7421562800759a8
Neil Turner <neil@neilturner.com>
2026-02-18 16:58:30 +0000

    Move prefs screen

---
+++ app/src/main/java/com/neilturner/aerialviews/ui/settings/OverlaysMetadataSlotFragment.kt
@@ -0,0 +1,20 @@
+package com.neilturner.aerialviews.ui.settings
+
+import android.os.Bundle
+import com.neilturner.aerialviews.R
+import com.neilturner.aerialviews.utils.FirebaseHelper
+import com.neilturner.aerialviews.utils.MenuStateFragment
+
+class OverlaysMetadataSlotFragment : MenuStateFragment() {
+    override fun onCreatePreferences(
+        savedInstanceState: Bundle?,
+        rootKey: String?,
+    ) {
+        setPreferencesFromResource(R.xml.settings_overlays_metadata_slot, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        FirebaseHelper.analyticsScreenView("Metadata Slot", this)
+    }
+}
