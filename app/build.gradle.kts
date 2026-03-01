267d3edd4f86ea1095c46f11d9d7bbb0d96eff04
Neil Turner <neilturner@gmail.com>
2026-03-01 17:13:51 +0000

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,9 +24,9 @@ android {
         applicationId = "com.neilturner.aerialviews"
         minSdk = 23 // Android v6
         targetSdk = 36
-        versionCode = 94
+        versionCode = 95
         versionName = "1.8.2"
-        betaVersion = "-beta4"
+        betaVersion = "-beta5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
