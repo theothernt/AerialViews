f4bfaeaff47963cd9c8577332bacca01fd46de69
Neil Turner <neil@neilturner.com>
2026-04-05 22:59:33 +0100

    Fix imports

--- app/src/common/java/utils/FirebaseHelper.kt
+++ app/src/common/java/utils/FirebaseHelper.kt
@@ -2,9 +2,9 @@ package com.neilturner.aerialviews.utils
 
 import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.crashlytics.ktx.crashlytics
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.analytics.analytics
+import com.google.firebase.crashlytics.crashlytics
+import com.google.firebase.Firebase
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
