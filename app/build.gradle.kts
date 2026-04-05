2647b165ec84b3ca09ac971258c2884638b1b404
Neil Turner <neil@neilturner.com>
2026-04-05 23:31:48 +0100

    Lib fixes and tidy up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -166,12 +166,26 @@ android {
 }
 
 dependencies {
+    val firebaseFlavors = listOf(
+        "github", "beta", "googleplay", "googleplaybeta", "amazon"
+    )
+
+    firebaseFlavors.forEach { flavor ->
+        "${flavor}Implementation"(platform(libs.firebase.bom))
+        "${flavor}Implementation"(libs.bundles.firebase)
+    }
     // Support all favors except F-Droid
-    "githubImplementation"(libs.bundles.firebase)
-    "betaImplementation"(libs.bundles.firebase)
-    "googleplayImplementation"(libs.bundles.firebase)
-    "googleplaybetaImplementation"(libs.bundles.firebase)
-    "amazonImplementation"(libs.bundles.firebase)
+//    "githubImplementation"(platform(libs.firebase.bom))
+//    "betaImplementation"(platform(libs.firebase.bom))
+//    "googleplayImplementation"(platform(libs.firebase.bom))
+//    "googleplaybetaImplementation"(platform(libs.firebase.bom))
+//    "amazonImplementation"(platform(libs.firebase.bom))
+//
+//    "githubImplementation"(libs.bundles.firebase)
+//    "betaImplementation"(libs.bundles.firebase)
+//    "googleplayImplementation"(libs.bundles.firebase)
+//    "googleplaybetaImplementation"(libs.bundles.firebase)
+//    "amazonImplementation"(libs.bundles.firebase)
 
     implementation(libs.bundles.kotlin)
     implementation(libs.bundles.androidx)
