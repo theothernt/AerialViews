76841e28894a380220d63c2d6839b13528af7b18
Neil Turner <neil@neilturner.com>
2026-05-03 17:38:01 +0100

    Bump target SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,14 +16,14 @@ plugins {
 
 android {
     namespace = "com.neilturner.aerialviews"
-    compileSdk = 36
+    compileSdk = 37
 
     var betaVersion = ""
     val keyProps = loadProperties("secrets.properties")
     defaultConfig {
         applicationId = "com.neilturner.aerialviews"
         minSdk = 23 // Android v6
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 117
         versionName = "1.8.3"
         betaVersion = "-beta5"
