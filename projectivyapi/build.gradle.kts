0f87c4e32a37e1376846a48ae8665bff5d4662d7
Neil Turner <neil@neilturner.com>
2026-08-11 17:58:55 +0100

    Build version tweak

--- projectivyapi/build.gradle.kts
+++ projectivyapi/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     namespace = "tv.projectivy.plugin.wallpaperprovider.api"
     compileSdk = 37
     defaultConfig {
-        minSdk = 22
+        minSdk = 23
     }
 
     buildFeatures {
